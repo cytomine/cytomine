b257668e9a6283d7436b302d3f864a6c3c74a563
sidhamed <siddiqhamed2017@gmail.com>
2025-08-21 15:16:29 +0200

    chore: add EqualsAndHashCode annotation and fix variable naming in ParameterSymlink for consistency

--- app-engine/src/main/java/be/cytomine/appengine/dto/handlers/scheduler/ParameterSymlink.java
+++ app-engine/src/main/java/be/cytomine/appengine/dto/handlers/scheduler/ParameterSymlink.java
@@ -1,11 +1,11 @@
 package be.cytomine.appengine.dto.handlers.scheduler;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Map;
-
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ParameterSymlink extends Symlink {
     private String parameterName;
-    private String Symlink;
+    private String symlink;
 }

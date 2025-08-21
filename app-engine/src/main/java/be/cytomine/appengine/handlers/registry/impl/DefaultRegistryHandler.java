859e94d726acaca0e2fdfdb882b8f40a804d5f3d
sidhamed <siddiqhamed2017@gmail.com>
2025-08-21 15:51:36 +0200

    chore: reformat pushImage method in DefaultRegistryHandler for consistent code style

--- app-engine/src/main/java/be/cytomine/appengine/handlers/registry/impl/DefaultRegistryHandler.java
+++ app-engine/src/main/java/be/cytomine/appengine/handlers/registry/impl/DefaultRegistryHandler.java
@@ -8,7 +8,8 @@ import be.cytomine.appengine.handlers.RegistryHandler;
 public class DefaultRegistryHandler implements RegistryHandler {
 
     @Override
-    public void pushImage(InputStream imageInputStream, String imageName) throws RegistryException {}
+    public void pushImage(InputStream imageInputStream, String imageName)
+        throws RegistryException {}
 
     @Override
     public void deleteImage(String imageName) throws RegistryException{}

d54830b481c1238ce35f31e194dbb462c36f6467
sidhamed <siddiqhamed2017@gmail.com>
2025-08-21 15:27:38 +0200

    chore: clean up imports in RegistryHandler for consistency

--- app-engine/src/main/java/be/cytomine/appengine/handlers/RegistryHandler.java
+++ app-engine/src/main/java/be/cytomine/appengine/handlers/RegistryHandler.java
@@ -2,7 +2,6 @@ package be.cytomine.appengine.handlers;
 
 import java.io.InputStream;
 
-import be.cytomine.appengine.dto.handlers.registry.DockerImage;
 import be.cytomine.appengine.exceptions.RegistryException;
 
 public interface RegistryHandler {

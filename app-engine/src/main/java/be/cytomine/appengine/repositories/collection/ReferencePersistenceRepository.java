f0175eb485a33a5e9e87c2c8b9308bb7e9c8b7c2
sidhamed <siddiqhamed2017@gmail.com>
2025-08-21 15:18:31 +0200

    chore: clean up imports in ReferencePersistenceRepository for consistency

--- app-engine/src/main/java/be/cytomine/appengine/repositories/collection/ReferencePersistenceRepository.java
+++ app-engine/src/main/java/be/cytomine/appengine/repositories/collection/ReferencePersistenceRepository.java
@@ -1,12 +1,12 @@
 package be.cytomine.appengine.repositories.collection;
 
-import be.cytomine.appengine.models.task.ParameterType;
-import be.cytomine.appengine.models.task.collection.ReferencePersistence;
-import be.cytomine.appengine.models.task.string.StringPersistence;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import be.cytomine.appengine.models.task.ParameterType;
+import be.cytomine.appengine.models.task.collection.ReferencePersistence;
 
 @Repository
 public interface ReferencePersistenceRepository extends JpaRepository<ReferencePersistence, UUID> {

e7e24c807d27f51f603b2e0eb23b22818b36c21c
sidhamed <siddiqhamed2017@gmail.com>
2025-08-28 16:43:49 +0200

    feat: add AppStoreController with task installation API for downloading and uploading tasks

---
+++ app-engine/src/main/java/be/cytomine/appengine/controllers/AppStoreController.java
@@ -0,0 +1,47 @@
+package be.cytomine.appengine.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import be.cytomine.appengine.dto.appstore.InstallRequest;
+import be.cytomine.appengine.dto.inputs.task.TaskDescription;
+import be.cytomine.appengine.exceptions.BundleArchiveException;
+import be.cytomine.appengine.exceptions.TaskServiceException;
+import be.cytomine.appengine.exceptions.ValidationException;
+import be.cytomine.appengine.services.AppStoreService;
+import be.cytomine.appengine.services.TaskService;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "${app-engine.api_prefix}${app-engine.api_version}/")
+public class AppStoreController {
+
+    private final AppStoreService appStoreService;
+    private final TaskService taskService;
+
+    @PostMapping("tasks/{scheme}/{host}/{namespace}/{version}")
+    public ResponseEntity<?> install(
+        @PathVariable String host,
+        @PathVariable String namespace,
+        @PathVariable String version,
+        @PathVariable String scheme)
+        throws IOException, TaskServiceException, ValidationException, BundleArchiveException {
+        log.info("Task install POST");
+        InstallRequest request = new InstallRequest(scheme, host, namespace, version);
+        File file = appStoreService.downloadTask(request);
+        assert file != null;
+        Optional<TaskDescription> description = taskService.uploadTask(new FileInputStream(file));
+        log.info("Task install POST end");
+        return ResponseEntity.ok(description);
+    }
+}

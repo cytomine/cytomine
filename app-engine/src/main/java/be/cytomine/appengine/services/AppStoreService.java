c0daa7d17b4079f5d63143cb03924e6b05d51990
sidhamed <siddiqhamed2017@gmail.com>
2025-08-28 16:43:59 +0200

    feat: add AppStoreService to handle task downloads from the AppStore

---
+++ app-engine/src/main/java/be/cytomine/appengine/services/AppStoreService.java
@@ -0,0 +1,45 @@
+package be.cytomine.appengine.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.UUID;
+
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import be.cytomine.appengine.dto.appstore.InstallRequest;
+
+
+@Service
+public class AppStoreService {
+
+
+    public File downloadTask(InstallRequest request)
+        throws IOException {
+
+        RestTemplate restTemplate = new RestTemplate();
+        Path tempPath = Path.of("bundle-" + UUID.randomUUID() + ".zip");
+        restTemplate.execute(
+                request.getAppStorescheme()
+                + "://"
+                + request.getAppStoreHost() + "/api/v1/tasks/{namespace}/{version}/bundle.zip",
+                org.springframework.http.HttpMethod.GET,
+                null,
+                clientHttpResponse -> {
+                    Files.copy(clientHttpResponse.getBody(), tempPath);
+                    return null;
+                },
+                request.getAppNamespace(),
+                request.getAppVersion()
+        );
+        return tempPath.toFile();
+    }
+}

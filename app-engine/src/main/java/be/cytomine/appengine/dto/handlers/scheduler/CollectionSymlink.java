dbcd1b9f99090413a04640fe04872d026400fbd1
sidhamed <siddiqhamed2017@gmail.com>
2025-09-04 11:50:05 +0200

    fix: add annotation

--- app-engine/src/main/java/be/cytomine/appengine/dto/handlers/scheduler/CollectionSymlink.java
+++ app-engine/src/main/java/be/cytomine/appengine/dto/handlers/scheduler/CollectionSymlink.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import lombok.Data;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class CollectionSymlink extends Symlink {
     private String parameterName;
     private Map<String, String> symlinks;

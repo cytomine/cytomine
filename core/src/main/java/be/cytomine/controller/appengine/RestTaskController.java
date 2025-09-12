781dedba5bab218ad18b926868fa9f604c563af6
Luca Di Carlo <luca.dicarlo@uliege.be>
2025-09-12 10:52:21 +0200

    fix: duplicate headers in proxy rest

--- core/src/main/java/be/cytomine/controller/appengine/RestTaskController.java
+++ core/src/main/java/be/cytomine/controller/appengine/RestTaskController.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,14 +31,14 @@ public class RestTaskController extends RestCytomineController {
     private AppEngineService appEngineService;
 
     @GetMapping("/tasks/{id}")
-    public ResponseEntity<String> descriptionById(
+    public String descriptionById(
             @PathVariable String id
     ) {
         return appEngineService.get("tasks/" + id);
     }
 
     @GetMapping("/tasks/{namespace}/{version}")
-    public ResponseEntity<String> description(
+    public String description(
             @PathVariable String namespace,
             @PathVariable String version
     ) {
@@ -45,12 +46,12 @@ public class RestTaskController extends RestCytomineController {
     }
 
     @GetMapping("/tasks")
-    public ResponseEntity<String> list() {
+    public String list() {
         return appEngineService.get("tasks");
     }
 
     @PostMapping("/tasks")
-    public ResponseEntity<String> upload(
+    public String upload(
             @RequestParam("task") MultipartFile task
     ) throws IOException {
         String name = UUID.randomUUID().toString();
@@ -63,14 +64,14 @@ public class RestTaskController extends RestCytomineController {
     }
 
     @GetMapping("/tasks/{id}/inputs")
-    public ResponseEntity<String> inputsById(
+    public String inputsById(
             @PathVariable UUID id
     ) {
         return appEngineService.get("tasks/" + id + "/inputs");
     }
-    
+
     @GetMapping("/tasks/{namespace}/{version}/inputs")
-    public ResponseEntity<String> inputs(
+    public String inputs(
             @PathVariable String namespace,
             @PathVariable String version
     ) {
@@ -78,14 +79,14 @@ public class RestTaskController extends RestCytomineController {
     }
 
     @GetMapping("/tasks/{id}/outputs")
-    public ResponseEntity<String> outputsById(
+    public String outputsById(
             @PathVariable UUID id
     ) {
         return appEngineService.get("tasks/" + id + "/outputs");
     }
 
     @GetMapping("/tasks/{namespace}/{version}/outputs")
-    public ResponseEntity<String> outputs(
+    public String outputs(
             @PathVariable String namespace,
             @PathVariable String version
     ) {
@@ -93,14 +94,14 @@ public class RestTaskController extends RestCytomineController {
     }
 
     @GetMapping("/tasks/{id}/descriptor.yml")
-    public ResponseEntity<String> descriptorById(
+    public String descriptorById(
             @PathVariable UUID id
     ) {
         return appEngineService.get("tasks/" + id + "/descriptor.yml");
     }
 
     @GetMapping("/tasks/{namespace}/{version}/descriptor.yml")
-    public ResponseEntity<String> descriptor(
+    public String descriptor(
             @PathVariable String namespace,
             @PathVariable String version
     ) {

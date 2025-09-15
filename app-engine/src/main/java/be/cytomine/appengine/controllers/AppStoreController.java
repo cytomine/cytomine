4746e05914ccdb861f0c9e1b2b5a9a6cbaac4322
sidhamed <siddiqhamed2017@gmail.com>
2025-09-15 12:29:22 +0200

    fix: reorder imports in AppStoreController

--- app-engine/src/main/java/be/cytomine/appengine/controllers/AppStoreController.java
+++ app-engine/src/main/java/be/cytomine/appengine/controllers/AppStoreController.java
@@ -7,8 +7,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,10 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import be.cytomine.appengine.models.store.AppStore;
-import be.cytomine.appengine.services.AppStoreService;
 import be.cytomine.appengine.exceptions.AppStoreNotFoundException;
 import be.cytomine.appengine.exceptions.ValidationException;
+import be.cytomine.appengine.models.store.AppStore;
+import be.cytomine.appengine.services.AppStoreService;
 
 @Slf4j
 @RequiredArgsConstructor

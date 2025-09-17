b6f0a0652242a0a112be1fe5e11bd8cea1ce9d58
sidhamed <siddiqhamed2017@gmail.com>
2025-09-17 16:53:53 +0200

    feat: add task installation endpoint and improve uploadTask method with streaming support

--- app-engine/src/main/java/be/cytomine/appengine/controllers/TaskController.java
+++ app-engine/src/main/java/be/cytomine/appengine/controllers/TaskController.java
@@ -1,6 +1,8 @@
 package be.cytomine.appengine.controllers;
 
+import java.io.IOException;
 import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +25,7 @@ import be.cytomine.appengine.dto.inputs.task.TaskDescription;
 import be.cytomine.appengine.dto.inputs.task.TaskRun;
 import be.cytomine.appengine.dto.responses.errors.ErrorBuilder;
 import be.cytomine.appengine.dto.responses.errors.ErrorCode;
+import be.cytomine.appengine.exceptions.AppStoreServiceException;
 import be.cytomine.appengine.exceptions.BundleArchiveException;
 import be.cytomine.appengine.exceptions.RunTaskServiceException;
 import be.cytomine.appengine.exceptions.TaskNotFoundException;
@@ -30,6 +33,7 @@ import be.cytomine.appengine.exceptions.TaskServiceException;
 import be.cytomine.appengine.exceptions.ValidationException;
 import be.cytomine.appengine.handlers.StorageData;
 import be.cytomine.appengine.models.task.Task;
+import be.cytomine.appengine.services.AppStoreService;
 import be.cytomine.appengine.services.TaskService;
 
 @Slf4j
@@ -39,13 +43,17 @@ import be.cytomine.appengine.services.TaskService;
 public class TaskController {
 
     private final TaskService taskService;
+    private final AppStoreService appStoreService;
 
     @PostMapping(path = "tasks")
     public ResponseEntity<?> upload(
         HttpServletRequest request
     ) throws TaskServiceException, ValidationException, BundleArchiveException {
         log.info("Task Upload POST");
-        Optional<TaskDescription> taskDescription = taskService.uploadTask(request);
+        // prepare for streaming
+        log.info("UploadTask: preparing streaming...");
+        InputStream inputStream = taskService.prepareStream(request);
+        Optional<TaskDescription> taskDescription = taskService.uploadTask(inputStream);
         log.info("Task Upload POST Ended");
         return ResponseEntity.ok(taskDescription);
     }
@@ -226,5 +234,20 @@ public class TaskController {
         TaskRun taskRun = taskService.createRunForTask(id);
         log.info("tasks/{id}/runs POST Ended");
         return ResponseEntity.ok(taskRun);
+    }
+
+    @PostMapping("tasks/{namespace}/{version}/install")
+    public ResponseEntity<?> install(
+        @PathVariable String namespace,
+        @PathVariable String version)
+        throws IOException,
+        TaskServiceException,
+        ValidationException,
+        BundleArchiveException,
+        AppStoreServiceException {
+        log.info("tasks/{scheme}/{host}/{namespace}/{version} POST");
+        Optional<TaskDescription> description = appStoreService.install(namespace, version);
+        log.info("tasks/{scheme}/{host}/{namespace}/{version} POST end");
+        return ResponseEntity.ok(description);
     }
 }

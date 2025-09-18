a3ef7b0694b28e4b6f1455f00cb106171dd92f75
Ba Thien LE <bathien.le.pro@gmail.com>
2025-09-18 17:30:15 +0200

    feat: add content wrapper

--- web-ui/ui/src/components/appengine/AppStoreList.vue
+++ web-ui/ui/src/components/appengine/AppStoreList.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="panel">
-    <p class="panel-heading">{{ $t('app-store') }}</p>
-    <section class="panel-block">
-      App Store List
-    </section>
+  <div class="content-wrapper">
+    <div class="panel">
+      <p class="panel-heading">{{ $t('app-store') }}</p>
+      <section class="panel-block">
+        App Store List
+      </section>
+    </div>
   </div>
 </template>
 

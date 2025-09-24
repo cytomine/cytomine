6d6cd5683d2686b4e393a57ab1d8e3110745c869
Ba Thien LE <bathien.le.pro@gmail.com>
2025-09-24 10:34:04 +0200

    test: fix AppStoreAddModal tests

--- web-ui/ui/tests/unit/components/appengine/AppStoreAddModal.test.js
+++ web-ui/ui/tests/unit/components/appengine/AppStoreAddModal.test.js
@@ -8,8 +8,9 @@ const localVue = createLocalVue();
 localVue.use(Buefy);
 
 describe('AppStoreAddModal.vue', () => {
-  const createWrapper = (options = {}) => {
-    return shallowMount(AppStoreAddModal, {
+  const createWrapper = () => shallowMount(
+    AppStoreAddModal,
+    {
       localVue,
       propsData: {
         active: true,
@@ -21,13 +22,12 @@ describe('AppStoreAddModal.vue', () => {
       stubs: {
         CytomineModal,
       },
-      ...options,
-    });
-  };
+    },
+  );
 
   it('should initialise with an empty store', () => {
     const wrapper = createWrapper();
-    expect(wrapper.vm.store).toEqual({name: '', host: '', default: false});
+    expect(wrapper.vm.store).toEqual({name: '', host: '', defaultStore: false});
   });
 
   it('should validate form correctly', () => {
@@ -35,25 +35,25 @@ describe('AppStoreAddModal.vue', () => {
 
     expect(wrapper.vm.isFormValid).toBeFalsy();
 
-    wrapper.setData({store: {name: 'Store', host: '', default: false}});
+    wrapper.setData({store: {name: 'Store', host: '', defaultStore: false}});
     expect(wrapper.vm.isFormValid).toBeFalsy();
 
-    wrapper.setData({store: {name: 'Store', host: 'http://host.com', default: false}});
+    wrapper.setData({store: {name: 'Store', host: 'http://host.com', defaultStore: false}});
     expect(wrapper.vm.isFormValid).toBeTruthy();
   });
 
   it('should emit add-store and resets form on add', async () => {
     const wrapper = createWrapper();
     const notifyMock = wrapper.vm.$notify;
-    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', default: true}});
+    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', defaultStore: true}});
 
     await wrapper.vm.add();
 
     expect(wrapper.emitted()['add-store'][0]).toEqual([
-      {name: 'Store1', host: 'http://host.com', default: true},
+      {name: 'Store1', host: 'http://host.com', defaultStore: true},
     ]);
     expect(wrapper.emitted()['update:active'][0]).toEqual([false]);
-    expect(wrapper.vm.store).toEqual({name: '', host: '', default: false});
+    expect(wrapper.vm.store).toEqual({name: '', host: '', defaultStore: false});
     expect(notifyMock).toHaveBeenCalledWith({
       type: 'success',
       text: 'notify-success-app-store-addition',
@@ -62,10 +62,10 @@ describe('AppStoreAddModal.vue', () => {
 
   it('should reset form and closes modal on cancel', async () => {
     const wrapper = createWrapper();
-    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', default: true}});
+    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', defaultStore: true}});
     await wrapper.vm.cancel();
 
-    expect(wrapper.vm.store).toEqual({name: '', host: '', default: false});
+    expect(wrapper.vm.store).toEqual({name: '', host: '', defaultStore: false});
     expect(wrapper.emitted()['update:active'][0]).toEqual([false]);
   });
 
@@ -73,7 +73,7 @@ describe('AppStoreAddModal.vue', () => {
     const wrapper = createWrapper();
     expect(wrapper.vm.isFormValid).toBeFalsy();
 
-    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', default: false}});
+    wrapper.setData({store: {name: 'Store1', host: 'http://host.com', defaultStore: false}});
     expect(wrapper.vm.isFormValid).toBeTruthy();
   });
 });

949e2083e66e7f59673e7509378016585ab98c7e
Luca Di Carlo <luca.dicarlo@uliege.be>
2025-09-26 14:34:30 +0200

    wip

--- app-engine/src/test/java/be/cytomine/appengine/AppEngineApplicationTests.java
+++ app-engine/src/test/java/be/cytomine/appengine/AppEngineApplicationTests.java
@@ -3,6 +3,8 @@ package be.cytomine.appengine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -14,7 +16,8 @@ class AppEngineApplicationTests {
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres =
-        new PostgreSQLContainer<>("postgres:16-alpine").withConnectTimeoutSeconds(60);
+        new PostgreSQLContainer<>("postgres:16-alpine").withPassword("appengine").withUsername(
+            "appengine").withConnectTimeoutSeconds(60);
 
     @Test
      void contextLoads() {

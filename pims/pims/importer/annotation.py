41576ec8c2412082821767bf84cc8dadbfa023ad
Ba Thien LE <bathien.le.pro@gmail.com>
2025-10-10 14:40:48 +0200

    refactor: AnnotationImporter

--- pims/pims/importer/annotation.py
+++ pims/pims/importer/annotation.py
@@ -6,14 +6,14 @@ from cytomine.models import Annotation
 
 
 class AnnotationImporter:
-    def __init__(self, annotation_path: Path):
-        self.annotation_path = annotation_path
+    def __init__(self, base_path: Path):
+        self.base_path = base_path
 
-    def load(self) -> Annotation | None:
+    def load(self, annotation_path: Path) -> Annotation | None:
         try:
-            with open(self.annotation_path, "r") as fp:
+            with open(self.base_path / annotation_path, "r") as fp:
                 geometry = geojson.load(fp)
 
             return Annotation(location=geometry)
-        except json.JSONDecodeError:
+        except (json.JSONDecodeError, FileNotFoundError):
             return None

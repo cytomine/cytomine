2da6977e3dcdc9bea9abcd6799ec24f8f9bbd3a4
Ba Thien LE <bathien.le.pro@gmail.com>
2025-10-24 11:37:31 +0200

    feat: add file upload item

---
+++ web-ui/src/components/appengine/FileUploadItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box">
+    <div class="columns is-vcentered is-gapless">
+      <div class="column is-narrow has-text-centered">
+        <b-icon icon="file-archive" size="is-large" />
+      </div>
+
+      <div class="column">
+        <div class="columns is-vcentered is-gapless">
+          <div class="column">
+            <div class="has-text-weight-semibold">{{ file.name }}</div>
+            <div class="has-text-grey">{{ formattedFileSize }}</div>
+          </div>
+
+          <div class="column is-narrow has-text-right">
+            <b-button icon-left="times" @click="$emit('file:remove', file)" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <b-button type="is-primary" size="is-medium">{{ $t('upload') }}</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import filesize from 'filesize';
+
+export default {
+  name: 'FileUploadItem',
+  props: {
+    file: {type: File, required: true},
+  },
+  computed: {
+    formattedFileSize() {
+      return this.file.size ? filesize(this.file.size, {base: 10}) : this.$t('unknown');
+    },
+  },
+};
+</script>

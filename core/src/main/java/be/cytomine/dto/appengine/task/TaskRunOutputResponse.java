fcbedec14c4aab54d08582563864f0806db00126
Ba Thien LE <bathien.le.pro@gmail.com>
2025-11-21 15:15:55 +0100

    refactor: configure jackson with snake case

--- core/src/main/java/be/cytomine/dto/appengine/task/TaskRunOutputResponse.java
+++ core/src/main/java/be/cytomine/dto/appengine/task/TaskRunOutputResponse.java
@@ -1,16 +1,15 @@
 package be.cytomine.dto.appengine.task;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record TaskRunOutputResponse(
     String id,
     String name,
-    @JsonProperty(value = "display_name") String displayName,
+    String displayName,
     String description,
     boolean optional,
     JsonNode type,
-    @JsonProperty(value = "derived_from") String derivedFrom
+    String derivedFrom
 ) {}

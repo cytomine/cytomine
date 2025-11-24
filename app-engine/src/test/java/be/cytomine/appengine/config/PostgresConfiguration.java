ce73b23c35a496652cf70495ca3c4645e75cb641
Luca Di Carlo <luca.dicarlo@uliege.be>
2025-11-24 15:47:44 +0100

    wip

--- app-engine/src/test/java/be/cytomine/appengine/config/PostgresConfiguration.java
+++ app-engine/src/test/java/be/cytomine/appengine/config/PostgresConfiguration.java
@@ -5,9 +5,5 @@ import org.testcontainers.junit.jupiter.Container;
 
 public class PostgresConfiguration {
     @Container
-    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14")
-        //.withUsername("appengine2")
-        //  .withDatabaseName("appengine2")
-        //      .withPassword("password2");
-        ;
+    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14");
 }

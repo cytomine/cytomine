c961e9a4c6a29e8ee6da0bb2885f895c5b92d760
Ba Thien LE <bathien.le.pro@gmail.com>
2025-12-10 15:07:47 +0100

    fix: add missing await

--- web-ui/src/components/appengine/sidebar/AppEngineSidebar.vue
+++ web-ui/src/components/appengine/sidebar/AppEngineSidebar.vue
@@ -93,7 +93,7 @@ export default {
 
         if (taskRun.state === 'FINISHED') {
           if (!taskRun.outputs) {
-            this.fetchOutputs(taskRun);
+            await this.fetchOutputs(taskRun);
           }
 
           if (taskRun.outputs.some(output => output.type === 'GEOMETRY')) {

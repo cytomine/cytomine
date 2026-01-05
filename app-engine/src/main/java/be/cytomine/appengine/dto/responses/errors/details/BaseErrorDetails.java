9b3eb315978dfb9bd8ea88302cf6622d3b6eecac
Ba Thien LE <bathien.le.pro@gmail.com>
2026-01-05 15:58:12 +0100

    fix: indentation

--- app-engine/src/main/java/be/cytomine/appengine/dto/responses/errors/details/BaseErrorDetails.java
+++ app-engine/src/main/java/be/cytomine/appengine/dto/responses/errors/details/BaseErrorDetails.java
@@ -7,12 +7,12 @@ import lombok.Data;
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = BatchError.class, name = "batch"),
-        @JsonSubTypes.Type(value = EmptyErrorDetails.class, name = "empty"),
-        @JsonSubTypes.Type(value = MultipleErrors.class, name = "multiple"),
-        @JsonSubTypes.Type(value = ParameterError.class, name = "parameter"),
-        @JsonSubTypes.Type(value = ParamRelatedError.class, name = "related"),
-        @JsonSubTypes.Type(value = ServerError.class, name = "server"),
+    @JsonSubTypes.Type(value = BatchError.class, name = "batch"),
+    @JsonSubTypes.Type(value = EmptyErrorDetails.class, name = "empty"),
+    @JsonSubTypes.Type(value = MultipleErrors.class, name = "multiple"),
+    @JsonSubTypes.Type(value = ParameterError.class, name = "parameter"),
+    @JsonSubTypes.Type(value = ParamRelatedError.class, name = "related"),
+    @JsonSubTypes.Type(value = ServerError.class, name = "server"),
 })
 public class BaseErrorDetails {
 }

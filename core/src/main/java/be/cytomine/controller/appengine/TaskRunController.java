96e0f3fd22d23b24633bce97dcca2430be1c079d
sidhamed <siddiqhamed2017@gmail.com>
2026-01-08 14:43:14 +0100

    fix: add auth parameter to task-runs endpoints and improve code formatting

--- core/src/main/java/be/cytomine/controller/appengine/TaskRunController.java
+++ core/src/main/java/be/cytomine/controller/appengine/TaskRunController.java
@@ -106,24 +106,29 @@ public class TaskRunController {
 
     @GetMapping("/project/{project}/task-runs/{task}/input/{parameter_name}")
     public void getTaskRunInputParameter(
-            @PathVariable Long project,
-            @PathVariable UUID task,
-            @PathVariable("parameter_name") String parameterName,
-            HttpServletResponse response
-    ) {
+        @PathVariable Long project,
+        @PathVariable UUID task,
+        @PathVariable("parameter_name") String parameterName,
+        @RequestParam(required = true) String auth,
+        HttpServletResponse response
+    )
+    {
 
         response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + parameterName + "\"");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\"" + parameterName + "\"");
         response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
         response.setHeader(HttpHeaders.PRAGMA, "no-cache");
         response.setHeader(HttpHeaders.EXPIRES, "0");
 
-        try (OutputStream os = response.getOutputStream()) {
+        try (OutputStream os = response.getOutputStream())
+        {
 
             taskRunService.getTaskRunIOParameter(project, task, parameterName, "input", os);
             os.flush();
 
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             throw new RuntimeException("Error while streaming the input parameter", e);
         }
     }
@@ -142,6 +147,7 @@ public class TaskRunController {
             @PathVariable Long project,
             @PathVariable UUID task,
             @PathVariable("parameter_name") String parameterName,
+            @RequestParam(required = true) String auth,
             HttpServletResponse response
     ) {
 

8fef10211f713112ad5d1f680bf87ce069b74a09
sidhamed <siddiqhamed2017@gmail.com>
2026-01-16 10:29:56 +0100

    fix: add regular refresh of annotation layers in `AppEngineSidebar` using `setInterval`

--- web-ui/src/components/appengine/sidebar/AppEngineSidebar.vue
+++ web-ui/src/components/appengine/sidebar/AppEngineSidebar.vue
@@ -88,6 +88,9 @@ export default {
         }
       }
     }, 2000);
+    setInterval(async () => {
+      this.$eventBus.$emit('annotation-layers:refresh');
+    }, 2000);
   },
   computed: {
     currentProject: get('currentProject/project'),

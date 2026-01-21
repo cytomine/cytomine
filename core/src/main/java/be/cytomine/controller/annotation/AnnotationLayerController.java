e985708f7e6bc7da6044bf8bfc70b833f403fc1e
sidhamed <siddiqhamed2017@gmail.com>
2026-01-21 11:28:58 +0100

    chore: remove unused imports from `AnnotationLayerController`

--- core/src/main/java/be/cytomine/controller/annotation/AnnotationLayerController.java
+++ core/src/main/java/be/cytomine/controller/annotation/AnnotationLayerController.java
@@ -1,7 +1,6 @@
 package be.cytomine.controller.annotation;
 
 import java.util.List;
-import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.cytomine.domain.annotation.Annotation;

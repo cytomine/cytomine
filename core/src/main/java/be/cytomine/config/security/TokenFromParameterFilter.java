e71d0a9132155576c6765d793bf2bff61c7dac77
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-01-23 11:32:03 +0100

    fix:  allow token extraction for annotations reports

--- core/src/main/java/be/cytomine/config/security/TokenFromParameterFilter.java
+++ core/src/main/java/be/cytomine/config/security/TokenFromParameterFilter.java
@@ -1,5 +1,11 @@
 package be.cytomine.config.security;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,26 +13,29 @@ import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
+import static java.util.function.Predicate.not;
 
 public class TokenFromParameterFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
         throws ServletException, IOException {
 
-        String authToken = request.getParameter("auth");
+        Optional<String> authToken =
+            Optional.ofNullable(request.getParameter("auth"))
+                .filter(not(String::isBlank))
+                .or(() -> Optional.ofNullable(request.getParameter("Authorization"))
+                              .filter(not(String::isBlank)))
+                .map(token -> token.startsWith("Bearer") ? token : "Bearer " + token);
 
-        if (authToken != null && request.getHeader("Authorization") == null) {
+        if (authToken.isPresent() && request.getHeader("Authorization") == null) {
             // Wrap the request to "fake" the Authorization header
             HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(request) {
                 @Override
                 public String getHeader(String name) {
                     if ("Authorization".equalsIgnoreCase(name)) {
-                        return "Bearer " + authToken;
+                        return authToken.get();
                     }
                     return super.getHeader(name);
                 }

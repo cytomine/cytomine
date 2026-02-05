8f7633b290275c902d6a3f12be4e2208c5cf0342
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-05 16:29:26 +0100

    wip

---
+++ e2e-tests/src/test/java/org/cytomine/e2e_tests/E2eTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cytomine.e2e_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e861e30c9846aa468537540192b9babfeaa3a9ec
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-09 15:14:47 +0100

    feat: create a login test

---
+++ e2e-tests/src/test/java/org/cytomine/e2etests/login/LoginTests.java
@@ -0,0 +1,26 @@
+package org.cytomine.e2etests.login;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+@RequiredArgsConstructor
+public class LoginTests {
+    final WebDriver driver = new ChromeDriver();
+
+    @Value("${ADMIN_USERNAME}")
+    final String adminUsername;
+
+    @Value("${ADMIN_PASSWORD}")
+    final String adminPassword;
+
+    @Test
+    void login() {
+
+    }
+}

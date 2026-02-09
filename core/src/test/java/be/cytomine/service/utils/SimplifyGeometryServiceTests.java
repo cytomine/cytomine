01e215acd19bea22cb72b220d977cffec65e27d9
Ba Thien LE <bathien.le.pro@gmail.com>
2026-02-09 14:36:59 +0100

    test: add testcontainers

--- core/src/test/java/be/cytomine/service/utils/SimplifyGeometryServiceTests.java
+++ core/src/test/java/be/cytomine/service/utils/SimplifyGeometryServiceTests.java
@@ -1,24 +1,10 @@
 package be.cytomine.service.utils;
 
-/*
-* Copyright (c) 2009-2022. Authors: see NOTICE file.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 import be.cytomine.BasicInstanceBuilder;
 import be.cytomine.CytomineCoreApplication;
 import be.cytomine.TestUtils;
+import be.cytomine.config.MongoTestConfiguration;
+import be.cytomine.config.PostGisTestConfiguration;
 import be.cytomine.domain.ontology.UserAnnotation;
 import be.cytomine.dto.annotation.SimplifiedAnnotation;
 
@@ -31,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import jakarta.transaction.Transactional;
@@ -40,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = CytomineCoreApplication.class)
 @AutoConfigureMockMvc
 @WithMockUser(authorities = "ROLE_SUPER_ADMIN", username = "superadmin")
+@Import({MongoTestConfiguration.class, PostGisTestConfiguration.class})
 @Transactional
 public class SimplifyGeometryServiceTests {
 
@@ -148,9 +136,7 @@ public class SimplifyGeometryServiceTests {
         SimplifiedAnnotation simplifiedAnnotation = simplifyGeometryService.simplifyPolygon(location, geometryCompression);
 
         assertThat(simplifiedAnnotation.getNewAnnotation().norm().toText()).isEqualTo(expected.toText());
-
     }
-
 
     public static int getPointMultiplyByGeometriesOrInteriorRings(Geometry geometry, long numberOfPoints){
         int result = 0;

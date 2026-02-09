29abc23a30b8e813d0599baf960bda0454a75517
Ba Thien LE <bathien.le.pro@gmail.com>
2026-02-09 11:58:27 +0100

    test: add postgis testcontainer

--- core/src/test/java/be/cytomine/CytomineCoreApplicationTests.java
+++ core/src/test/java/be/cytomine/CytomineCoreApplicationTests.java
@@ -2,8 +2,12 @@ package be.cytomine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import be.cytomine.integration.config.PostGisConfiguration;
+
+@Import({PostGisConfiguration.class})
 @SpringBootTest
 @ActiveProfiles("test")
 class CytomineCoreApplicationTests {

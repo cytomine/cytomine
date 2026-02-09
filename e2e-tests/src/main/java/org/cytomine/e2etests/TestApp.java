bb98308d77f73cc311cf60fd9157db00428cde7d
Luca <luca.dicarlo@uliege.be>
2026-02-09 13:16:10 +0100

    feat: e2e tests init (#612)

---
+++ e2e-tests/src/main/java/org/cytomine/e2etests/TestApp.java
@@ -0,0 +1,13 @@
+package org.cytomine.e2etests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApp.class, args);
+	}
+
+}

76636993f0cfdbf17dd303a5c89980399b715d15
Ba Thien LE <bathien.le.pro@gmail.com>
2026-02-10 17:00:49 +0100

    fix: test

--- core/src/test/java/be/cytomine/CytomineCoreApplicationTests.java
+++ core/src/test/java/be/cytomine/CytomineCoreApplicationTests.java
@@ -1,15 +1,15 @@
 package be.cytomine;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import be.cytomine.config.MongoTestConfiguration;
 import be.cytomine.config.PostGisTestConfiguration;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(classes = CytomineCoreApplication.class)
+@AutoConfigureMockMvc
 @Import({MongoTestConfiguration.class, PostGisTestConfiguration.class})
 class CytomineCoreApplicationTests {
 

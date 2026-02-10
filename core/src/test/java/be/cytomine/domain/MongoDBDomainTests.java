c1eee0bb4914e494c3cbbaba15de0c890a46559f
Ba Thien LE <bathien.le.pro@gmail.com>
2026-02-10 13:41:48 +0100

    refactor: domain tests

--- core/src/test/java/be/cytomine/domain/MongoDBDomainTests.java
+++ core/src/test/java/be/cytomine/domain/MongoDBDomainTests.java
@@ -244,7 +244,7 @@ public class MongoDBDomainTests {
     }
 
     @Test
-    void peristent_connection_indexes() {
+    void persistent_connection_indexes() {
         ListIndexesIterable<Document> indexes = retrieveIndex("persistentConnection");
 
         Document indexId = null;
@@ -265,130 +265,115 @@ public class MongoDBDomainTests {
         assertThat(((Document)indexUserCreated.get("key")).get("created")).isEqualTo(-1);
     }
 
-    /**
-     * Check that the format of MongoDB is the same as
-     */
     @Test
-    void persistent_connection_domain() throws ParseException, JsonProcessingException {
-        PersistentConnection connection = new PersistentConnection();
-        connection.setId(3073L);
-        connection.setCreated(mongoDBFormat.parse("2021-09-22T09:06:32.472Z"));
-        connection.setSession("B7850470EED8CD7570E05C50FD5F02F6");
-        connection.setProject(null);
+    void persistent_connection_domain() throws ParseException {
+        Long expectedId = 3073L;
+        Date expectedCreated = mongoDBFormat.parse("2021-09-22T09:06:32.472Z");
+        String expectedSession = "B7850470EED8CD7570E05C50FD5F02F6";
+        Long expectedProject = null;
 
+        PersistentConnection connection = new PersistentConnection();
+        connection.setId(expectedId);
+        connection.setCreated(expectedCreated);
+        connection.setSession(expectedSession);
+        connection.setProject(expectedProject);
         connection = persistentConnectionRepository.insert(connection);
 
         Document document = retrieveDocument("persistentConnection", connection.getId());
 
-        String expectedResults =
-                "{\n" +
-                        "   \"_id\":3073,\n" +
-                        "   \"created\":{\n" +
-                        "      \"$date\":\"2021-09-22T07:06:32.472Z\"\n" + //UTC
-                        "   },\n" +
-                        "   \"session\":\"B7850470EED8CD7570E05C50FD5F02F6\"\n" +
-                        "}";
-
-        String expectedResultsAnotherTimeZone = expectedResults.replaceAll("2021-09-22T07:06:32.472Z", "2021-09-22T09:06:32.472Z");
-
-        assertThat(objectMapper.readTree(document.toJson())).isIn(objectMapper.readTree(expectedResults), objectMapper.readTree(expectedResultsAnotherTimeZone));
+        assertThat(document.getLong("_id")).isEqualTo(expectedId);
+        assertThat(document.getDate("created")).isEqualTo(expectedCreated);
+        assertThat(document.getString("session")).isEqualTo(expectedSession);
+        assertThat(document.get("project")).isEqualTo(expectedProject);
     }
 
-
-    /**
-     * Check that the format of MongoDB is the same as
-     */
     @Test
-    void persistent_project_connection_domain() throws ParseException, JsonProcessingException {
-        PersistentProjectConnection connection = new PersistentProjectConnection();
-        connection.setId(3073L);
-        connection.setBrowser("firefox");
-        connection.setBrowserVersion("92.0.0");
-        connection.setCreated(mongoDBFormat.parse("2021-09-22T09:06:32.472Z"));
-        connection.setOs("Linux");
-        connection.setProject(3063L);
-        connection.setSession("B7850470EED8CD7570E05C50FD5F02F6");
-        connection.setUser(58L);
-        connection.setCountCreatedAnnotations(0);
-        connection.setCountViewedImages(0);
-        connection.setTime(139164L);
+    void persistent_project_connection_domain() throws ParseException {
+        Long expectedId = 3073L;
+        String expectedBrowser = "firefox";
+        String expectedBrowserVersion = "92.0.0";
+        Date expectedCreated = mongoDBFormat.parse("2021-09-22T09:06:32.472Z");
+        String expectedOs = "Linux";
+        Long expectedProject = 3063L;
+        String expectedSession = "B7850470EED8CD7570E05C50FD5F02F6";
+        Long expectedUser = 58L;
+        Integer expectedCountCreatedAnnotations = 0;
+        Integer expectedCountViewedImages = 0;
+        Long expectedTime = 139164L;
 
+        PersistentProjectConnection connection = new PersistentProjectConnection();
+        connection.setId(expectedId);
+        connection.setBrowser(expectedBrowser);
+        connection.setBrowserVersion(expectedBrowserVersion);
+        connection.setCreated(expectedCreated);
+        connection.setOs(expectedOs);
+        connection.setProject(expectedProject);
+        connection.setSession(expectedSession);
+        connection.setUser(expectedUser);
+        connection.setCountCreatedAnnotations(expectedCountCreatedAnnotations);
+        connection.setCountViewedImages(expectedCountViewedImages);
+        connection.setTime(expectedTime);
         connection = persistentProjectConnectionRepository.insert(connection);
 
         Document document = retrieveDocument("persistentProjectConnection", connection.getId());
 
-        String expectedResults =
-                "{\n" +
-                        "   \"_id\":3073,\n" +
-                        "   \"browser\":\"firefox\",\n" +
-                        "   \"browserVersion\":\"92.0.0\",\n" +
-                        "   \"created\":{\n" +
-                        "      \"$date\":\"2021-09-22T07:06:32.472Z\"\n" + //utc
-                        "   },\n" +
-                        "   \"os\":\"Linux\",\n" +
-                        "   \"project\":3063,\n" +
-                        "   \"session\":\"B7850470EED8CD7570E05C50FD5F02F6\",\n" +
-                        "   \"user\":58,\n" +
-                        "   \"countCreatedAnnotations\":0,\n" +
-                        "   \"countViewedImages\":0,\n" +
-                        "   \"time\":139164\n" +
-                        "}";
-
-        String expectedResultsAnotherTimeZone = expectedResults.replaceAll("2021-09-22T07:06:32.472Z","2021-09-22T09:06:32.472Z");
-
-        assertThat(objectMapper.readTree(document.toJson())).isIn(objectMapper.readTree(expectedResults), objectMapper.readTree(expectedResultsAnotherTimeZone));
-        // TODO: test index
+        assertThat(document.getLong("_id")).isEqualTo(expectedId);
+        assertThat(document.getString("browser")).isEqualTo(expectedBrowser);
+        assertThat(document.getString("browserVersion")).isEqualTo(expectedBrowserVersion);
+        assertThat(document.getDate("created")).isEqualTo(expectedCreated);
+        assertThat(document.getString("os")).isEqualTo(expectedOs);
+        assertThat(document.getLong("project")).isEqualTo(expectedProject);
+        assertThat(document.getString("session")).isEqualTo(expectedSession);
+        assertThat(document.getLong("user")).isEqualTo(expectedUser);
+        assertThat(document.getInteger("countCreatedAnnotations")).isEqualTo(expectedCountCreatedAnnotations);
+        assertThat(document.getInteger("countViewedImages")).isEqualTo(expectedCountViewedImages);
+        assertThat(document.getLong("time")).isEqualTo(expectedTime);
     }
 
-
-    /**
-     * Check that the format of MongoDB is the same as
-     */
     @Test
-    void persistent_image_consultation_domain() throws ParseException, JsonProcessingException {
-        PersistentImageConsultation consultation = new PersistentImageConsultation();
-        consultation.setId(3975L);
-        consultation.setUser(58L);
-        consultation.setImage(3962L);
-        consultation.setProject(3063L);
-        consultation.setSession("B6AC04394B9D9F746A15E511C5DC243B");
-        consultation.setProjectConnection(3974L);
-        consultation.setCreated(mongoDBFormat.parse("2021-09-23T08:55:02.602Z"));
-        consultation.setSession("B6AC04394B9D9F746A15E511C5DC243B");
-        consultation.setMode("view");
-        consultation.setImageName("CMU-1-Small-Region (1).svs");
-        consultation.setImageThumb("http://localhost-core/api/imageinstance/3962/thumb.png?maxSize=256");
-        consultation.setTime(12149L);
-        consultation.setCountCreatedAnnotations(0);
+    void persistent_image_consultation_domain() throws ParseException {
+        Long expectedId = 3975L;
+        Long expectedUser = 58L;
+        Long expectedImage = 3962L;
+        Long expectedProject = 3063L;
+        Long expectedProjectConnection = 3974L;
+        String expectedSession = "B6AC04394B9D9F746A15E511C5DC243B";
+        Date expectedCreated = mongoDBFormat.parse("2021-09-23T08:55:02.602Z");
+        String expectedMode = "view";
+        String expectedImageName = "CMU-1-Small-Region (1).svs";
+        String expectedImageThumb = "http://localhost-core/api/imageinstance/3962/thumb.png?maxSize=256";
+        Long expectedTime = 12149L;
+        Integer expectedCountCreatedAnnotations = 0;
 
+        PersistentImageConsultation consultation = new PersistentImageConsultation();
+        consultation.setId(expectedId);
+        consultation.setUser(expectedUser);
+        consultation.setImage(expectedImage);
+        consultation.setProject(expectedProject);
+        consultation.setProjectConnection(expectedProjectConnection);
+        consultation.setSession(expectedSession);
+        consultation.setCreated(expectedCreated);
+        consultation.setMode(expectedMode);
+        consultation.setImageName(expectedImageName);
+        consultation.setImageThumb(expectedImageThumb);
+        consultation.setTime(expectedTime);
+        consultation.setCountCreatedAnnotations(expectedCountCreatedAnnotations);
         consultation = persistentImageConsultationRepository.insert(consultation);
 
         Document document = retrieveDocument("persistentImageConsultation", consultation.getId());
 
-        String expectedResults =
-                "{\n" +
-                        "\"_id\": 3975,\n" +
-                        "\"created\": {\n" +
-                        "\"$date\": \"2021-09-23T06:55:02.602Z\"\n" + //UTC
-                        "},\n" +
-                        "\"image\": 3962,\n" +
-                        "\"imageName\": \"CMU-1-Small-Region (1).svs\",\n" +
-                        "\"imageThumb\": \"http://localhost-core/api/imageinstance/3962/thumb.png?maxSize=256\",\n" +
-                        "\"mode\": \"view\",\n" +
-                        "\"project\": 3063,\n" +
-                        "\"projectConnection\": 3974,\n" +
-                        "\"session\": \"B6AC04394B9D9F746A15E511C5DC243B\",\n" +
-                        "\"user\": 58,\n" +
-                        "\"countCreatedAnnotations\": 0,\n" +
-                        "\"time\": 12149\n" +
-                        "}";
-
-        String expectedResultsAnotherTimeZone = expectedResults.replaceAll("2021-09-23T06:55:02.602Z", "2021-09-23T08:55:02.602Z");
-
-        assertThat(objectMapper.readTree(document.toJson())).isIn(objectMapper.readTree(expectedResults), objectMapper.readTree(expectedResultsAnotherTimeZone));
-        // TODO: issue with Date: created seems to have issue with UTC
-
-        // TODO: test index
+        assertThat(document.getLong("_id")).isEqualTo(expectedId);
+        assertThat(document.getLong("user")).isEqualTo(expectedUser);
+        assertThat(document.getLong("image")).isEqualTo(expectedImage);
+        assertThat(document.getLong("project")).isEqualTo(expectedProject);
+        assertThat(document.getLong("projectConnection")).isEqualTo(expectedProjectConnection);
+        assertThat(document.getString("session")).isEqualTo(expectedSession);
+        assertThat(document.getDate("created")).isEqualTo(expectedCreated);
+        assertThat(document.getString("mode")).isEqualTo(expectedMode);
+        assertThat(document.getString("imageName")).isEqualTo(expectedImageName);
+        assertThat(document.getString("imageThumb")).isEqualTo(expectedImageThumb);
+        assertThat(document.getLong("time")).isEqualTo(expectedTime);
+        assertThat(document.getInteger("countCreatedAnnotations")).isEqualTo(expectedCountCreatedAnnotations);
     }
 
     /**
@@ -437,79 +422,60 @@ public class MongoDBDomainTests {
         assertThat(((Document)image.get("key")).get("image")).isEqualTo(1);
     }
 
-    /**
-     * Check that the format of MongoDB is the same as
-     */
     @Test
-    void persistent_user_position_domain() throws ParseException, JsonProcessingException {
-        ImageInstance imageInstance = new ImageInstance();
-        imageInstance.setId(3962L);
-        Project project = new Project();
-        project.setId(3063L);
-        SliceInstance sliceInstance = new SliceInstance();
-        sliceInstance.setId(3963L);
-        User user = new User();
-        user.setId(58L);
+    void persistent_user_position_domain() throws ParseException {
+        Long expectedId = 3977L;
+        boolean expectedBroadcast = false;
+        Date expectedCreated = mongoDBFormat.parse("2021-09-23T08:55:03.608Z");
+        Long expectedImage = 3962L;
+        String expectedImageName = "CMU-1-Small-Region (1).svs";
+        List<List<Double>> expectedLocation = new AreaDTO(
+                new be.cytomine.dto.image.Point(-3338d, 3128d),
+                new be.cytomine.dto.image.Point(5558d, 3128d),
+                new be.cytomine.dto.image.Point(5558d, -160d),
+                new be.cytomine.dto.image.Point(-3338d, -160d)
+        ).toMongodbLocation().getCoordinates();
+        Long expectedProject = 3063L;
+        Double expectedRotation = 0d;
+        String expectedSession = "B6AC04394B9D9F746A15E511C5DC243B";
+        Long expectedSlice = 3963L;
+        Long expectedUser = 58L;
+        Integer expectedZoom = 2;
 
         PersistentUserPosition lastPosition = new PersistentUserPosition();
-        lastPosition.setId(3977L);
-        lastPosition.setBroadcast(false);
-        lastPosition.setCreated(mongoDBFormat.parse("2021-09-23T08:55:03.608Z"));
-        lastPosition.setImage(imageInstance.getId());
-        lastPosition.setImageName("CMU-1-Small-Region (1).svs");
-        lastPosition.setLocation(new AreaDTO(
-                new be.cytomine.dto.image.Point(-3338d,3128d),
-                new be.cytomine.dto.image.Point(5558d,3128d),
-                new be.cytomine.dto.image.Point(5558d,-160d),
-                new be.cytomine.dto.image.Point(-3338d,-160d)
-        ).toMongodbLocation().getCoordinates());
-        lastPosition.setProject(project.getId());
-        lastPosition.setRotation(0d);
-        lastPosition.setSession("B6AC04394B9D9F746A15E511C5DC243B");
-        lastPosition.setSlice(sliceInstance.getId());
-        lastPosition.setUser(user.getId());
-        lastPosition.setZoom(2);
-
+        lastPosition.setId(expectedId);
+        lastPosition.setBroadcast(expectedBroadcast);
+        lastPosition.setCreated(expectedCreated);
+        lastPosition.setImage(expectedImage);
+        lastPosition.setImageName(expectedImageName);
+        lastPosition.setLocation(expectedLocation);
+        lastPosition.setProject(expectedProject);
+        lastPosition.setRotation(expectedRotation);
+        lastPosition.setSession(expectedSession);
+        lastPosition.setSlice(expectedSlice);
+        lastPosition.setUser(expectedUser);
+        lastPosition.setZoom(expectedZoom);
         lastPosition = persistentUserPositionRepository.insert(lastPosition);
 
         Document document = retrieveDocument("persistentUserPosition", lastPosition.getId());
 
-        MongoCollection<Document> persistentProjectConnectionFromGrails = mongoClient.getDatabase(mongoDatabaseName).getCollection("persistentUserPosition");
-        ArrayList<Document> results = persistentProjectConnectionFromGrails.find(eq("_id", 3977L))
-                .into(new ArrayList<>());
-        for (Document result : results) {
-            System.out.println(result.toJson());
-        }
+        assertThat(document.getLong("_id")).isEqualTo(expectedId);
+        assertThat(document.getBoolean("broadcast")).isEqualTo(expectedBroadcast);
+        assertThat(document.getDate("created")).isEqualTo(expectedCreated);
+        assertThat(document.getLong("image")).isEqualTo(expectedImage);
+        assertThat(document.getString("imageName")).isEqualTo(expectedImageName);
 
-        String expectedResults =
-                "{\n" +
-                        "\"_id\": 3977,\n" +
-                        "\"broadcast\": false,\n" +
-                        "\"created\": {\n" +
-                        "\"$date\": \"2021-09-23T06:55:03.608Z\"\n" + // //UTC
-                        "},\n" +
-                        "\"image\": 3962,\n" +
-                        "\"imageName\": \"CMU-1-Small-Region (1).svs\",\n" +
-                        "\"location\": [\n" +
-                        "[-3338.0, 3128.0],\n" +
-                        "[5558.0, 3128.0],\n" +
-                        "[5558.0, -160.0],\n" +
-                        "[-3338.0, -160.0]\n" +
-                        "],\n" +
-                        "\"project\": 3063,\n" +
-                        "\"rotation\": 0.0,\n" +
-                        "\"session\": \"B6AC04394B9D9F746A15E511C5DC243B\",\n" +
-                        "\"slice\": 3963,\n" +
-                        "\"user\": 58,\n" +
-                        "\"zoom\": 2\n" +
-                        "}";
+        @SuppressWarnings("unchecked")
+        List<List<Double>> actualLocation = (List<List<Double>>) document.get("location");
+        assertThat(actualLocation).isEqualTo(expectedLocation);
 
-        String expectedResultsAnotherTimeZone = expectedResults.replaceAll("2021-09-23T06:55:03.608Z", "2021-09-23T08:55:03.608Z");
-
-        assertThat(objectMapper.readTree(document.toJson())).isIn(objectMapper.readTree(expectedResults), objectMapper.readTree(expectedResultsAnotherTimeZone));
-       //Fails because rotation is a string
+        assertThat(document.getLong("project")).isEqualTo(expectedProject);
+        assertThat(document.getDouble("rotation")).isEqualTo(expectedRotation);
+        assertThat(document.getString("session")).isEqualTo(expectedSession);
+        assertThat(document.getLong("slice")).isEqualTo(expectedSlice);
+        assertThat(document.getLong("user")).isEqualTo(expectedUser);
+        assertThat(document.getInteger("zoom")).isEqualTo(expectedZoom);
     }
-
 
     /**
      * Check that the format of MongoDB is the same as

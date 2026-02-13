a943a9d2af70c146df1de7a9d3cb721983453754
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-13 15:17:41 +0100

    wip

---
+++ e2e-tests/src/test/java/org/cytomine/e2etests/configuration/WebDriverConfig.java
@@ -0,0 +1,14 @@
+package org.cytomine.e2etests.configuration;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebDriverConfig {
+
+    WebDriver webDriver(){
+
+    }
+
+
+}

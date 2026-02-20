c5021b15b9bb239799a8a772471d964b9e09dca6
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-20 11:26:21 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/configuration/SeleniumDriver.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/configuration/SeleniumDriver.java
@@ -1,7 +1,6 @@
 package org.cytomine.e2etests.configuration;
 
 import java.net.URL;
-import java.time.Duration;
 import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,11 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.http.ClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import static java.net.http.HttpClient.Version.HTTP_1_1;
 
 @Slf4j
 @Component
@@ -28,17 +24,9 @@ public class SeleniumDriver {
             seleniumUrl.map(url -> {
                     log.info("Instantiated RemoteWebDriver with url: {}", url);
                     options.addArguments("--headless");
-
-                    // Configure HTTP client with proper timeouts for k3s environment
-                    ClientConfig clientConfig = ClientConfig.defaultConfig()
-                                                    .connectionTimeout(Duration.ofSeconds(30))
-                                                    .readTimeout(Duration.ofMinutes(3))
-                                                    .version(HTTP_1_1.toString());
-
                     return RemoteWebDriver.builder()
                                .address(url)
                                .oneOf(options)
-                               .config(clientConfig)
                                .build();
                 })
                 .orElseGet(() ->

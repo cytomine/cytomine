ec1f64bfe88862566e8fa08fbc3a404f6f6744df
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-24 14:05:06 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
@@ -1,10 +1,11 @@
 package org.cytomine.e2etests.project;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.net.URL;
 import java.time.Duration;
 import java.util.Set;
 import java.util.UUID;
-
 import org.cytomine.e2etests.configuration.SeleniumDriver;
 import org.cytomine.e2etests.ui.CytomineSteps;
 import org.cytomine.e2etests.ui.WebDriverUtils;
@@ -19,76 +20,72 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-import static java.util.stream.Collectors.toSet;
-
 @Import({SeleniumDriver.class, CytomineSteps.class, WebDriverUtils.class})
 @SpringBootTest
 public class CytomineTests {
-    @Autowired
-    SeleniumDriver driverProvider;
-    WebDriver driver;
-    Wait<WebDriver> wait;
+  @Autowired SeleniumDriver driverProvider;
+  WebDriver driver;
+  Wait<WebDriver> wait;
 
-    @Value("${cytomine.url}")
-    URL cytomineUrl;
+  @Value("${cytomine.url}")
+  URL cytomineUrl;
 
-    @Value("${cytomine.admin.username}")
-    String adminUsername;
+  @Value("${cytomine.admin.username}")
+  String adminUsername;
 
-    @Value("${cytomine.admin.password}")
-    String adminPassword;
+  @Value("${cytomine.admin.password}")
+  String adminPassword;
 
-    @Autowired
-    CytomineSteps cytomineSteps;
+  @Autowired CytomineSteps cytomineSteps;
 
-    @BeforeEach
-    void setUp() {
-        driver = driverProvider.driver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-    }
+  @BeforeEach
+  void setUp() {
+    driver = driverProvider.driver();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+  }
 
-    @AfterEach
-    void tearDown() {
-        driver.close();
-    }
+  @AfterEach
+  void tearDown() {
+    driver.close();
+  }
 
-    @Test
-    void login() {
-        cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
-    }
+  @Test
+  void login() {
+    cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
+  }
 
-    @Test
-    void createProject() {
-        String projectName = "selenium-" + UUID.randomUUID();
-        cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
-        String projectURL = cytomineSteps.createProject(wait, driver, cytomineUrl, projectName);
-        cytomineSteps.deleteProject(wait, projectURL);
-    }
+  @Test
+  void createProject() {
+    String projectName = "selenium-" + UUID.randomUUID();
+    cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
+    String projectURL = cytomineSteps.createProject(wait, driver, cytomineUrl, projectName);
+    cytomineSteps.deleteProject(wait, projectURL);
+  }
 
+  @Test
+  void deleteProject() {
+    String projectName = "selenium-" + UUID.randomUUID();
+    cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
+    String projectURL = cytomineSteps.createProject(wait, driver, cytomineUrl, projectName);
+    cytomineSteps.deleteProject(wait, projectURL);
+  }
 
-    @Test
-    void deleteProject() {
-        String projectName = "selenium-" + UUID.randomUUID();
-        cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
-        String projectURL = cytomineSteps.createProject(wait, driver, cytomineUrl, projectName);
-        cytomineSteps.deleteProject(wait, projectURL);
-    }
-
-    @Test
-    void listProjects() {
-        Set<String> projectNames = Set.of(
+  @Test
+  void listProjects() {
+    Set<String> projectNames =
+        Set.of(
             "selenium-" + UUID.randomUUID(),
             "selenium-" + UUID.randomUUID(),
-            "selenium-" + UUID.randomUUID()
-        );
-        cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
-        Set<String> projectUrls =
-            projectNames.stream().map(name -> cytomineSteps.createProject(wait, driver,
-                cytomineUrl, name)).collect(toSet());
-        cytomineSteps.listProjects(wait, cytomineUrl, projectNames);
-        Set<String> ignored =
-            projectUrls.stream().map(projectUrl -> cytomineSteps.deleteProject(wait,
-                projectUrl)).collect(toSet());
-    }
-
+            "selenium-" + UUID.randomUUID());
+    cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
+    Set<String> projectUrls =
+        projectNames.stream()
+            .map(name -> cytomineSteps.createProject(wait, driver, cytomineUrl, name))
+            .collect(toSet());
+    cytomineSteps.listProjects(wait, cytomineUrl, projectNames);
+    Set<String> ignored =
+        projectUrls.stream()
+            .map(projectUrl -> cytomineSteps.deleteProject(wait, projectUrl))
+            .collect(toSet());
+  }
 }

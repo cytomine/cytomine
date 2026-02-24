e48b62475247f2d41d9aa8b97d24ff5794ef382c
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-24 17:24:19 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/ui/WebDriverUtils.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/ui/WebDriverUtils.java
@@ -9,50 +9,53 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebDriverUtils {
 
-  void xpathClick(Wait<WebDriver> wait, String xpath) {
-    byClick(wait, By.xpath(xpath));
-  }
+    void xpathClick(Wait<WebDriver> wait, String xpath) {
+        byClick(wait, By.xpath(xpath));
+    }
 
-  void byClick(Wait<WebDriver> wait, By by) {
-    byIsDisplayed(wait, by);
-    wait.until(
-        d -> {
-          d.findElement(by).click();
-          return true;
-        });
-  }
+    void byClick(Wait<WebDriver> wait, By by) {
+        byIsDisplayed(wait, by);
+        wait.until(
+                d -> {
+                    d.findElement(by)
+                            .click();
+                    return true;
+                });
+    }
 
-  void bySendKeys(Wait<WebDriver> wait, By by, String keys) {
-    byIsDisplayed(wait, by);
-    wait.until(
-        d -> {
-          d.findElement(by).sendKeys(keys);
-          return true;
-        });
-  }
+    void bySendKeys(Wait<WebDriver> wait, By by, String keys) {
+        byIsDisplayed(wait, by);
+        wait.until(
+                d -> {
+                    d.findElement(by)
+                            .sendKeys(keys);
+                    return true;
+                });
+    }
 
-  void goTo(Wait<WebDriver> wait, String url) {
-    wait.until(
-        d -> {
-          d.get(url);
-          return true;
-        });
-  }
+    void goTo(Wait<WebDriver> wait, String url) {
+        wait.until(
+                d -> {
+                    d.get(url);
+                    return true;
+                });
+    }
 
-  @SneakyThrows
-  boolean byIsDisplayed(Wait<WebDriver> wait, By by) {
-    waitLoading(wait);
-    wait.until(d -> d.findElement(by).isDisplayed());
-    Thread.sleep(500);
-    return true;
-  }
+    @SneakyThrows
+    boolean byIsDisplayed(Wait<WebDriver> wait, By by) {
+        Thread.sleep(500);
+        waitLoading(wait);
+        wait.until(d -> d.findElement(by)
+                .isDisplayed());
+        return true;
+    }
 
-  @SneakyThrows
-  void waitLoading(Wait<WebDriver> wait) {
-    wait.until(
-        d -> {
-          var loadingOverlays = d.findElements(By.cssSelector(".loading-overlay.is-active"));
-          return loadingOverlays.isEmpty();
-        });
-  }
+    @SneakyThrows
+    void waitLoading(Wait<WebDriver> wait) {
+        wait.until(
+                d -> {
+                    var loadingOverlays = d.findElements(By.cssSelector(".loading-overlay.is-active"));
+                    return loadingOverlays.isEmpty();
+                });
+    }
 }

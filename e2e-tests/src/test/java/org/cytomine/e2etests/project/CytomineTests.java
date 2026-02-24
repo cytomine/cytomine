c3fa893e9bfc1f20b192ed96e1d4c107a9f64a96
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-02-24 18:36:22 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
@@ -1,11 +1,14 @@
 package org.cytomine.e2etests.project;
 
+import lombok.SneakyThrows;
 import org.cytomine.e2etests.configuration.SeleniumDriver;
 import org.cytomine.e2etests.ui.CytomineSteps;
 import org.cytomine.e2etests.ui.WebDriverUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,9 +17,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.UUID;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @Import({SeleniumDriver.class, CytomineSteps.class, WebDriverUtils.class})
 @SpringBootTest
@@ -45,7 +54,17 @@ public class CytomineTests {
     }
 
     @AfterEach
+    @SneakyThrows
     void tearDown() {
+
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);  // Capture the screenshot as a file
+        System.out.println("Screenshot captured at: " + screenshot.getAbsolutePath());
+
+        // Define the destination path for saving the screenshot
+        Path destination = Paths.get("./build/reports/" + screenshot.getName());
+        Files.createDirectories(Path.of("./build/reports/"));
+        // Move the screenshot to the desired location
+        Files.move(screenshot.toPath(), destination, REPLACE_EXISTING);
         driver.close();
     }
 
@@ -61,7 +80,6 @@ public class CytomineTests {
         cytomineSteps.createProject(wait, cytomineUrl, projectName);
         cytomineSteps.deleteProject(wait, driver.getCurrentUrl());
     }
-
 
     @Test
     void deleteProject() {

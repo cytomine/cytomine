cfe37390c13a5cc8ddfd7f9119bfc0098d48b57e
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-03 11:17:11 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/project/CytomineTests.java
@@ -1,13 +1,23 @@
 package org.cytomine.e2etests.project;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Duration;
-import java.util.Set;
+import lombok.SneakyThrows;
+import org.cytomine.e2etests.configuration.SeleniumDriver;
+import org.cytomine.e2etests.ui.CytomineSteps;
+import org.cytomine.e2etests.ui.WebDriverUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -19,27 +29,8 @@ import java.time.Duration;
 import java.util.Optional;
 import java.util.Set;
 
-import lombok.SneakyThrows;
-import org.cytomine.e2etests.configuration.SeleniumDriver;
-import org.cytomine.e2etests.ui.CytomineSteps;
-import org.cytomine.e2etests.ui.WebDriverUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-import static java.nio.file.attribute.PosixFilePermission.GROUP_READ;
-import static java.nio.file.attribute.PosixFilePermission.OTHERS_READ;
-import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
+import static java.nio.file.attribute.PosixFilePermission.*;
 import static java.util.UUID.randomUUID;
 import static java.util.stream.Collectors.toSet;
 import static org.openqa.selenium.OutputType.FILE;
@@ -67,14 +58,14 @@ public class CytomineTests {
     @BeforeEach
     void setUp() {
         driver = driverProvider.driver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @AfterEach
     void tearDown(TestInfo testInfo) {
         saveScreenshot("closing-" + testInfo.getTestMethod()
-                                        .map(Method::getName)
-                                        .orElseGet(() -> "no-name-" + randomUUID()));
+                .map(Method::getName)
+                .orElseGet(() -> "no-name-" + randomUUID()));
         driver.close();
     }
 
@@ -107,27 +98,27 @@ public class CytomineTests {
     @Test
     void listProjects() {
         Set<String> projectNames =
-            Set.of(
-                "selenium-" + randomUUID(),
-                "selenium-" + randomUUID(),
-                "selenium-" + randomUUID());
+                Set.of(
+                        "selenium-" + randomUUID(),
+                        "selenium-" + randomUUID(),
+                        "selenium-" + randomUUID());
         cytomineSteps.login(wait, cytomineUrl, adminUsername, adminPassword);
         Set<String> projectUrls =
-            projectNames.stream()
-                .map(name -> cytomineSteps.createProject(wait, driver, cytomineUrl, name))
-                .collect(toSet());
+                projectNames.stream()
+                        .map(name -> cytomineSteps.createProject(wait, driver, cytomineUrl, name))
+                        .collect(toSet());
         cytomineSteps.listProjects(wait, cytomineUrl, projectNames);
         Set<String> ignored =
-            projectUrls.stream()
-                .map(projectURL -> {
-                    String ontologyURL = cytomineSteps.getOntologyUrlFromProject(wait, projectURL);
-                    cytomineSteps.deleteProject(wait, projectURL);
-                    if (ontologyURL != null) {
-                        cytomineSteps.deleteOntology(wait, ontologyURL);
-                    }
-                    return projectURL;
-                })
-                .collect(toSet());
+                projectUrls.stream()
+                        .map(projectURL -> {
+                            String ontologyURL = cytomineSteps.getOntologyUrlFromProject(wait, projectURL);
+                            cytomineSteps.deleteProject(wait, projectURL);
+                            if (ontologyURL != null) {
+                                cytomineSteps.deleteOntology(wait, ontologyURL);
+                            }
+                            return projectURL;
+                        })
+                        .collect(toSet());
     }
 
     @Test

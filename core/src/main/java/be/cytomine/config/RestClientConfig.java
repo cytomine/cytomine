5b394ce7738f6a72cf4c7fbec9f65f33a79595b8
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-09 10:50:29 +0100

    feat: 721 - new persistence layer

--- core/src/main/java/be/cytomine/config/RestClientConfig.java
+++ core/src/main/java/be/cytomine/config/RestClientConfig.java
@@ -2,7 +2,6 @@ package be.cytomine.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,9 +10,5 @@ public class RestClientConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-    @Bean
-    public RestClient restClient() {
-        return RestClient.create("http://0.0.0.0:8081/");
     }
 }

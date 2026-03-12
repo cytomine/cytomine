88ae42f1c9d335952141791138608866b4022c7e
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-12 16:00:25 +0100

    wip

--- app-engine/src/main/java/be/cytomine/appengine/config/SecurityConfig.java
+++ app-engine/src/main/java/be/cytomine/appengine/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package be.cytomine.appengine.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +29,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf((csrf) -> csrf.disable());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf((csrf) -> csrf.disable());
         return http.build();
     }
 }

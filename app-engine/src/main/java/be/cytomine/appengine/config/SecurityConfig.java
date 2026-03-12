65b14ce9f41a2cec67e3ddf3264873e9edb1e7e4
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-12 15:47:49 +0100

    wip

--- app-engine/src/main/java/be/cytomine/appengine/config/SecurityConfig.java
+++ app-engine/src/main/java/be/cytomine/appengine/config/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf((csrf) -> csrf.disable());
         return http.build();
     }

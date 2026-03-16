3ac4fe3fd8b17cc05ba490d9bb0b19f4f9518b53
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-16 14:08:56 +0100

    wip

--- repository/src/main/java/org/cytomine/repository/persistence/entity/TermEntity.java
+++ repository/src/main/java/org/cytomine/repository/persistence/entity/TermEntity.java
@@ -4,18 +4,19 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
-import lombok.Generated;
 
 @Entity(name = "term")
 @Data
 public class TermEntity {
     @Id
-    @Generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private Long version;

bb370ae78bbf201da8467c32f5dce868c8805743
Ba Thien LE <bathien.le.pro@gmail.com>
2026-03-17 13:56:52 +0100

    style: fix curly bracket style

--- core/src/main/java/be/cytomine/service/utils/KmeansGeometryService.java
+++ core/src/main/java/be/cytomine/service/utils/KmeansGeometryService.java
@@ -152,14 +152,21 @@ public class KmeansGeometryService {
     }
 
     public Integer getRuleForNumberOfAnnotations(long annotations, Map<Integer, Integer> ruleLine) {
-        if (annotations >= ANNOTATIONSIZE5) return ruleLine.get(ANNOTATIONSIZE5);
-        if (annotations >= ANNOTATIONSIZE4) return ruleLine.get(ANNOTATIONSIZE4);
-        if (annotations >= ANNOTATIONSIZE3) return ruleLine.get(ANNOTATIONSIZE3);
-        if (annotations >= ANNOTATIONSIZE2) return ruleLine.get(ANNOTATIONSIZE2);
-        if (annotations >= ANNOTATIONSIZE1) return ruleLine.get(ANNOTATIONSIZE1);
+        if (annotations >= ANNOTATIONSIZE5) {
+            return ruleLine.get(ANNOTATIONSIZE5);
+        }
+        if (annotations >= ANNOTATIONSIZE4) {
+            return ruleLine.get(ANNOTATIONSIZE4);
+        }
+        if (annotations >= ANNOTATIONSIZE3) {
+            return ruleLine.get(ANNOTATIONSIZE3);
+        }
+        if (annotations >= ANNOTATIONSIZE2) {
+            return ruleLine.get(ANNOTATIONSIZE2);
+        }
+        if (annotations >= ANNOTATIONSIZE1) {
+            return ruleLine.get(ANNOTATIONSIZE1);
+        }
         throw new WrongArgumentException("Cannot find rule for annotations count = " + annotations);
     }
-
-
-
 }

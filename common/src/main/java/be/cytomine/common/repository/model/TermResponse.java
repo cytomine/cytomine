27cb87186abb50524ef05d347c702efb4a92a2c4
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-18 12:08:11 +0100

    wip

--- common/src/main/java/be/cytomine/common/repository/model/TermResponse.java
+++ common/src/main/java/be/cytomine/common/repository/model/TermResponse.java
@@ -2,7 +2,8 @@ package be.cytomine.common.repository.model;
 
 import java.util.Set;
 
-public record TermResponse(long id, String name, String color, long ontologyId, long projectId, Set<TermResponse> children) {
+public record TermResponse(long id, String name, String color, long ontologyId, long projectId,
+                           Set<TermResponse> children) {
     public TermResponse {
         if (children == null) {
             children = Set.of();

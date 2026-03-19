5729f486d9b71c80415872282d7772019c426426
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-19 16:55:18 +0100

    wip

--- e2e-tests/entrypoint.sh
+++ e2e-tests/entrypoint.sh
@@ -14,5 +14,5 @@ until wget -qO- "${SELENIUM}/wd/hub/status" 2>/dev/null |grep -q '"ready": true'
 done
 
 echo "Selenium is ready. Waiting 10 seconds to be sure"
-sleep 15
+sleep 40
 exec gradle :e2e-tests:test --no-daemon

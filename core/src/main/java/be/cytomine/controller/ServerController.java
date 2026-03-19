659776866af4f421de57efa1a3ee955d0d81bb82
Ba Thien Le <bathien.le.pro@gmail.com>
2026-03-19 11:59:19 +0100

    refactor(core): ping method (#772)

--- core/src/main/java/be/cytomine/controller/ServerController.java
+++ core/src/main/java/be/cytomine/controller/ServerController.java
@@ -1,107 +1,34 @@
 package be.cytomine.controller;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.stream.Stream;
+import java.util.Map;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.NonTransientDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
 
 import be.cytomine.config.properties.ApplicationProperties;
-import be.cytomine.domain.security.User;
-import be.cytomine.domain.social.LastConnection;
-import be.cytomine.domain.social.PersistentConnection;
-import be.cytomine.repositorynosql.social.LastConnectionRepository;
-import be.cytomine.repositorynosql.social.PersistentConnectionRepository;
-import be.cytomine.service.database.SequenceService;
 import be.cytomine.utils.JsonObject;
 
-@RestController
-@RequestMapping("")
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("")
+@RestController
 public class ServerController extends RestCytomineController {
 
     private final ApplicationProperties applicationProperties;
 
-    private final SequenceService sequenceService;
-
-    private final PersistentConnectionRepository persistentConnectionRepository;
-
-    private final LastConnectionRepository lastConnectionRepository;
-
-    @RequestMapping(value = {"/server/ping.json", "/server/ping"}, method = {RequestMethod.GET, RequestMethod.POST}) // without.json is deprecated
-    public ResponseEntity<String> ping(HttpSession session) throws IOException {
-        log.debug("REST request to ping");
-        JsonObject json = super.mergeQueryParamsAndBodyParams();
-        JsonObject response = new JsonObject();
-        response.put("alive", true);
-        response.put("version", applicationProperties.getVersion());
-        response.put("serverURL", applicationProperties.getServerURL());
-        response.put("serverID", applicationProperties.getServerId());
-
-        // TODO 2024.2 - LAST CONNECTION (IN A PROJECT)
-//        if (isAuthenticated()) {
-//            User user = currentUserService.getCurrentUser();
-//            response.put("user", user.getId());
-////            response.put("shortTermToken", tokenProvider.createToken(SecurityContextHolder.getContext().getAuthentication(), TokenType.SHORT_TERM));
-//
-////            if (!user.getEnabled()) {
-////                log.info("Disabled user. Invalidation of its sessions");
-////                session.invalidate();
-////            }
-//            Long idProject = null;
-//            if(!json.getJSONAttrStr("project", "null").equals("null")) {
-//                idProject = json.getJSONAttrLong("project");
-//            }
-//            addLastConnection(user, idProject);
-//        }
-        return JsonResponseEntity.status(HttpStatus.OK).body(response.toJsonString());
-    }
-
-    public static boolean isAuthenticated() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication != null &&
-                getAuthorities(authentication).noneMatch("ROLE_ANONYMOUS"::equals);
-    }
-
-    private static Stream<String> getAuthorities(Authentication authentication) {
-        return authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority);
-    }
-
-    void addLastConnection(User user, Long idProject) {
-        try {
-            LastConnection connection = new LastConnection();
-            connection.setId(sequenceService.generateID());
-            connection.setUser(user.getId());
-            connection.setDate(new Date());
-            connection.setCreated(connection.getDate());
-            connection.setProject(idProject);
-            lastConnectionRepository.insert(connection); //don't use save (stateless collection)
-
-            PersistentConnection connectionPersist = new PersistentConnection();
-            connectionPersist.setId(sequenceService.generateID());
-            connectionPersist.setUser(user.getId());
-            connectionPersist.setProject(idProject);
-            connectionPersist.setCreated(new Date());
-            connectionPersist.setSession(RequestContextHolder.currentRequestAttributes().getSessionId());
-            persistentConnectionRepository.insert(connectionPersist); //don't use save (stateless collection)
-        } catch (NonTransientDataAccessException e) {
-            log.error(e.getMessage());
-        }
+    @GetMapping("/server/ping")
+    public Map<String, Object> ping() {
+        log.debug("GET /server/ping");
+        return Map.of(
+            "alive", true,
+            "version", applicationProperties.getVersion(),
+            "serverURL", applicationProperties.getServerURL(),
+            "serverID", applicationProperties.getServerId()
+        );
     }
 
     @GetMapping("/status.json")

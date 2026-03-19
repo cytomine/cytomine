624d43f351a2a6e93d4636a22fba19b7cb34325b
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-19 13:11:36 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/configuration/SeleniumDriver.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/configuration/SeleniumDriver.java
@@ -22,19 +22,20 @@ public class SeleniumDriver {
     public WebDriver driver() {
         FirefoxOptions options = new FirefoxOptions();
         WebDriver webDriver = seleniumUrl.map(url -> {
-                log.info("Instantiated RemoteWebDriver with url: {}", url);
-                options.addArguments("--headless");
-                RemoteWebDriver driver = (RemoteWebDriver) RemoteWebDriver.builder()
-                    .address(url)
-                    .oneOf(options)
-                    .build();
-                driver.setFileDetector(new LocalFileDetector());
-                return driver;
-            })
-            .orElseGet(() -> {
-                log.info("Instantiated FirefoxDriver");
-                return new FirefoxDriver(options);
-            });
+            log.info("Instantiated RemoteWebDriver with url: {}", url);
+            options.addArguments("--headless");
+            RemoteWebDriver driver = (RemoteWebDriver) RemoteWebDriver.builder().address(url).oneOf(options).build();
+            driver.setFileDetector(new LocalFileDetector());
+            return driver;
+        }).orElseGet(() -> {
+            log.info("Instantiated FirefoxDriver");
+            return new FirefoxDriver(options);
+        });
+        // This does not work. I am leaving it here so that we know.
+        // https://www.selenium.dev/documentation/webdriver/waits/ <- I could not make it work.
+        //        webDriver.manage()
+        //                .timeouts()
+        //                .implicitlyWait(Duration.ofSeconds(10));
         return webDriver;
     }
 }

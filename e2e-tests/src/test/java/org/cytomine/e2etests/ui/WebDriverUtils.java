7dac5eb8ddc9032f5c195767c91a788d9d238e8f
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-03-20 14:52:03 +0100

    wip

--- e2e-tests/src/test/java/org/cytomine/e2etests/ui/WebDriverUtils.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/ui/WebDriverUtils.java
@@ -64,7 +64,7 @@ public class WebDriverUtils {
         waitLoading(wait);
         wait.until(d -> {
             try {
-                return ExpectedConditions.visibilityOfElementLocated(by).apply(d);
+                return ExpectedConditions.visibilityOfElementLocated(by);
             } catch (Exception e) {
                 return false;
             }

6a37b12d5de8368a6011dc1ce496eff2a302e1ad
sidhamed <siddiqhamed2017@gmail.com>
2026-03-24 12:14:21 +0100

    fix: imports

--- core/src/main/java/be/cytomine/service/security/UserService.java
+++ core/src/main/java/be/cytomine/service/security/UserService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;

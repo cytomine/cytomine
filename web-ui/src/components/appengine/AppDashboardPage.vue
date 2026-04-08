79d40ec5ae35649fd833c9504636aedab5c8a37a
Ba Thien LE <bathien.le.pro@gmail.com>
2026-04-08 13:24:25 +0200

    feat: add table with dummy data

--- web-ui/src/components/appengine/AppDashboardPage.vue
+++ web-ui/src/components/appengine/AppDashboardPage.vue
@@ -1,3 +1,55 @@
 <template>
-  <div>App Dashboard Page</div>
+  <div class="content-wrapper">
+    <div class="panel">
+      <p class="panel-heading">{{ $t('app-dashboard') }}</p>
+
+      <section class="panel-block">
+        <b-table :data="data" :columns="columns"></b-table>
+      </section>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'AppDashboardPage',
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          appName: 'Stardist (1.0.0)',
+          user: 'John Doe',
+          created: '2016-10-15 13:43:27',
+          status: 'Finished',
+          action: 'View logs',
+        },
+      ],
+      columns: [
+        {
+          field: 'appName',
+          label: 'App Name',
+        },
+        {
+          field: 'user',
+          label: 'Launched By',
+        },
+        {
+          field: 'created',
+          label: 'Execution Date',
+        },
+        {
+          field: 'status',
+          label: 'Status',
+          centered: true,
+        },
+        {
+          field: 'action',
+          label: 'Action',
+          centered: true,
+        },
+      ],
+    };
+  },
+};
+</script>

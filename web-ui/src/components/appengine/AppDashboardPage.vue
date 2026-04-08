74ddce7bdbe55499d261ef4c38d2d41cc5910d98
Ba Thien LE <bathien.le.pro@gmail.com>
2026-04-08 14:04:20 +0200

    feat: add color scheme for statuses

--- web-ui/src/components/appengine/AppDashboardPage.vue
+++ web-ui/src/components/appengine/AppDashboardPage.vue
@@ -19,7 +19,9 @@
             </b-table-column>
 
             <b-table-column :label="$t('status')" centered>
-              {{ run.status }}
+              <span class="tag" :class="statusClass(run.status)">
+                {{ run.status }}
+              </span>
             </b-table-column>
 
             <b-table-column :label="$t('actions')" centered>
@@ -42,16 +44,31 @@ export default {
   data() {
     return {
       data: [
-        {
-          id: 1,
-          name: 'Stardist (1.0.0)',
-          user: 'John Doe',
-          date: '2016-10-15 13:43:27',
-          status: 'Finished',
-          action: 'View logs',
-        },
+        {id: 1, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:27', status: 'Finished'},
+        {id: 12, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:28', status: 'Failed'},
+        {id: 3, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:29', status: 'Running'},
+        {id: 4, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:30', status: 'Pending'},
+        {id: 5, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:31', status: 'Queuing'},
+        {id: 6, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:32', status: 'Queued'},
+        {id: 7, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:33', status: 'Created'},
+        {id: 8, name: 'Stardist (1.0.0)', user: 'John Doe', date: '2016-10-15 13:43:34', status: 'Provisioned'},
       ],
     };
+  },
+  methods: {
+    statusClass(status) {
+      const map = {
+        created: 'is-light',
+        provisioned: 'is-info is-light',
+        queuing: 'is-warning is-light',
+        queued: 'is-warning',
+        running: 'is-primary',
+        pending: 'is-warning is-light',
+        failed: 'is-danger',
+        finished: 'is-success',
+      };
+      return map[status.toLowerCase()] ?? 'is-light';
+    },
   },
 };
 </script>

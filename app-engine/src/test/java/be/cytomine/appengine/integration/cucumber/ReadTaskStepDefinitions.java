b13932dd34bcdd5d4e20cb7a443d3cc549229d90
Ba Thien LE <bathien.le.pro@gmail.com>
2026-04-13 13:43:20 +0200

    style: fix lint

--- app-engine/src/test/java/be/cytomine/appengine/integration/cucumber/ReadTaskStepDefinitions.java
+++ app-engine/src/test/java/be/cytomine/appengine/integration/cucumber/ReadTaskStepDefinitions.java
@@ -98,24 +98,25 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a set of valid tasks has been successfully uploaded")
-    public void a_set_of_valid_tasks_has_been_successfully_uploaded() {
+    public void validTasksAreUploaded() {
         taskRepository.save(TestTaskBuilder.buildHardcodedAddInteger());
         taskRepository.save(TestTaskBuilder.buildHardcodedSubtractInteger());
     }
 
     @When("user calls the endpoint {string} \\(excluding version prefix, e.g. {string}) with HTTP method {string}")
-    public void user_calls_the_endpoint_excluding_version_prefix_e_g_with_http_method(String uri, String string2, String method) {
+    public void userCallsEndpointWithMethod(String uri, String string2, String method) {
         tasks = apiClient.getTasks();
     }
 
     @Then("App Engine retrieves relevant data from the database")
-    public void app_engine_retrieves_relevant_data_from_the_database() {
+    public void appEngineRetrievesData() {
         Assertions.assertNotNull(tasks);
         Assertions.assertEquals(2, tasks.size());
     }
 
-    @Then("App Engine sends a {string} OK response with a payload containing the descriptions of the available tasks as a JSON payload \\(see OpenAPI spec)")
-    public void app_engine_sends_a_ok_response_with_a_payload_containing_the_descriptions_of_the_available_tasks_as_a_json_payload_see_open_api_spec(String string) {
+    @Then("App Engine sends a {string} OK response with a payload containing the descriptions "
+        + "of the available tasks as a JSON payload \\(see OpenAPI spec)")
+    public void appEngineReturnsTaskListAsJson(String string) {
         for (TaskDescription description : tasks) {
             Assertions.assertNotNull(description.getDescription());
             Assertions.assertNotNull(description.getNamespace());
@@ -142,7 +143,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a valid task has a {string}, a {string} has been successfully uploaded")
-    public void a_valid_has_a_a_has_been_successfully_uploaded(String namespace, String version) throws FileStorageException {
+    public void taskWithNamespaceAndVersionIsUploaded(String namespace, String version) throws FileStorageException {
         taskRepository.deleteAll();
         String bundleFilename = namespace + "-" + version + ".zip";
         persistedTask = TestTaskBuilder.buildTaskFromResource(bundleFilename);
@@ -156,7 +157,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a valid task has a {string} has been successfully uploaded")
-    public void a_valid_has_a_has_been_successfully_uploaded(String uuid) throws FileStorageException {
+    public void taskWithUuidIsUploaded(String uuid) throws FileStorageException {
         taskRepository.deleteAll();
         String bundleFilename = "com.cytomine.dummy.arithmetic.integer.subtraction-1.0.0.zip";
         persistedTask = TestTaskBuilder.buildTaskFromResource(bundleFilename, UUID.fromString(uuid));
@@ -171,7 +172,11 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a valid task has a {string}, a {string} and {string} has been successfully uploaded")
-    public void a_valid_task_with_namespace_and_version_and_uuid_successfully_uploaded(String namespace, String version, String uuid) throws FileStorageException {
+    public void taskWithNamespaceVersionAndUuidIsUploaded(
+        String namespace,
+        String version,
+        String uuid
+    ) throws FileStorageException {
         taskRepository.deleteAll();
         String bundleFilename = namespace + "-" + version + ".zip";
         persistedTask = TestTaskBuilder.buildTaskFromResource(bundleFilename, UUID.fromString(uuid));
@@ -186,39 +191,40 @@ public class ReadTaskStepDefinitions {
     }
 
     @Then("App Engine retrieves task with {string}, a {string}  from the database")
-    public void app_engine_retrieves_task_data_from_the_database(String namespace, String version) {
+    public void appEngineRetrievesTaskByNamespaceAndVersion(String namespace, String version) {
     }
 
     @Then("App Engine retrieves task inputs with {string}, a {string}  from the database")
-    public void app_engine_retrieves_task_inputs_data_from_the_database(String namespace, String version) {
+    public void appEngineRetrievesTaskInputsByNamespaceAndVersion(String namespace, String version) {
         Assertions.assertNotNull(persistedInputs);
         Assertions.assertFalse(persistedInputs.isEmpty());
     }
 
     @Then("App Engine retrieves task with {string} from the database")
-    public void app_engine_retrieves_task_data_from_the_database(String uuid) {
+    public void appEngineRetrievesTaskByUuid(String uuid) {
     }
 
     @Then("App Engine retrieves task inputs with {string} from the database")
-    public void app_engine_retrieves_task_inputs_data_from_the_database(String uuid) {
+    public void appEngineRetrievesTaskInputsByUuid(String uuid) {
         // a Input Parameters is received
         Assertions.assertNotNull(persistedInputs);
         Assertions.assertFalse(persistedInputs.isEmpty());
-        Assertions.assertEquals(persistedInputs.size(), 2);
+        Assertions.assertEquals(2, persistedInputs.size());
     }
 
     @When("user calls the endpoint {string} with {string} and {string} with HTTP method GET")
-    public void user_calls_the_endpoint_with_namespace_and_version_http_method_get(String uri, String namespace, String version) {
+    public void userCallsEndpointWithNamespaceAndVersion(String uri, String namespace, String version) {
         persistedTaskDescription = apiClient.getTask(namespace, version);
     }
 
     @When("user calls the endpoint {string} with id {string} HTTP method GET")
-    public void user_calls_the_endpoint_with_uuid_http_method_get(String uri, String uuid) {
+    public void userCallsEndpointWithUuid(String uri, String uuid) {
         persistedTaskDescription = apiClient.getTask(uuid);
     }
 
-    @Then("App Engine sends a {string} OK response with a payload containing the task description as a JSON payload \\(see OpenAPI spec)")
-    public void app_engine_sends_a_ok_response_with_a_payload_containing_the_task_description_as_a_json_payload_see_open_api_spec(String string) {
+    @Then("App Engine sends a {string} OK response with a payload containing "
+        + "the task description as a JSON payload \\(see OpenAPI spec)")
+    public void appEngineReturnsTaskDescriptionAsJson(String string) {
         Assertions.assertNotNull(persistedTaskDescription);
         Assertions.assertEquals(persistedTaskDescription.getNamespace(), persistedTask.getNamespace());
         Assertions.assertEquals(persistedTaskDescription.getVersion(), persistedTask.getVersion());
@@ -226,8 +232,9 @@ public class ReadTaskStepDefinitions {
         Assertions.assertEquals(persistedTaskDescription.getAuthors().size(), persistedTask.getAuthors().size());
     }
 
-    @Then("App Engine sends a {string} OK response with a payload containing the task inputs as a JSON payload \\(see OpenAPI spec)")
-    public void app_engine_sends_a_ok_response_with_a_payload_containing_the_task_inputs_as_a_json_payload_see_open_api_spec(String string) {
+    @Then("App Engine sends a {string} OK response with a payload containing "
+        + "the task inputs as a JSON payload \\(see OpenAPI spec)")
+    public void appEngineReturnsTaskInputsAsJson(String string) {
         Assertions.assertNotNull(persistedInputs);
         Set<Parameter> persistedTaskInputs = persistedTask
             .getParameters()
@@ -238,32 +245,35 @@ public class ReadTaskStepDefinitions {
     }
 
     @When("user calls the endpoint {string} with {string} and {string} HTTP method GET")
-    public void user_calls_the_endpoint_with_and_http_method_get(String endpoint, String namespace, String version) {
+    public void userCallsHttpEndpointWithNamespaceAndVersion(String endpoint, String namespace, String version) {
         persistedInputs = apiClient.getInputs(namespace, version);
     }
 
     @When("user calls the outputs endpoint {string} with {string} and {string} HTTP method GET")
-    public void user_calls_the_outputs_endpoint_with_and_http_method_get(String endpoint, String namespace, String version) {
+    public void userCallsOutputsEndpointWithNamespaceAndVersion(String endpoint, String namespace, String version) {
         persistedOutputs = apiClient.getOutputs(namespace, version);
     }
 
     @When("user calls the endpoint {string} with {string} HTTP method GET")
-    public void user_calls_the_endpoint_with_http_method_get(String endpoint, String uuid) {
+    public void userCallsHttpEndpointWithUuid(String endpoint, String uuid) {
         persistedInputs = apiClient.getInputs(uuid);
     }
 
     @When("user calls the outputs endpoint {string} with {string} HTTP method GET")
-    public void user_calls_the_outputs_endpoint_with_http_method_get(String endpoint, String uuid) {
+    public void userCallsOutputsEndpointWithUuid(String endpoint, String uuid) {
         persistedOutputs = apiClient.getOutputs(uuid);
     }
 
     @When("user calls the download endpoint with {string} and {string} with HTTP method GET")
-    public void user_calls_the_download_endpoint_with_and_with_http_method_get(String namespace, String version) {
+    public void userCallsDownloadEndpointWithNamespaceAndVersion(String namespace, String version) {
         persistedDescriptor = apiClient.getTaskDescriptor(namespace, version);
     }
 
     @Given("the task descriptor is stored in the file storage service in storage {string} under filename {string}")
-    public void the_task_descriptor_is_stored_in_the_file_storage_service_in_storage_under_filename(String storageReference, String descriptorFileName) throws FileStorageException, IOException {
+    public void taskDescriptorIsStoredInFileStorage(
+        String storageReference,
+        String descriptorFileName
+    ) throws FileStorageException, IOException {
         // save it in file storage service
         Storage storage = new Storage(persistedTask.getStorageReference());
         Assertions.assertTrue(storageHandler.checkStorageExists(storage));
@@ -279,18 +289,18 @@ public class ReadTaskStepDefinitions {
     }
 
     @When("user calls the download endpoint with {string} with HTTP method GET")
-    public void user_calls_the_download_endpoint_with_with_http_method_get(String uuid) {
+    public void userCallsDownloadEndpointWithUuid(String uuid) {
         persistedDescriptor = apiClient.getTaskDescriptor(uuid);
     }
 
     @Then("App Engine retrieves the descriptor file {string} from the file storage")
-    public void app_engine_retrieves_the_descriptor_file_from_the_file_storage(String fileName) {
+    public void appEngineRetrievesDescriptorFromStorage(String fileName) {
         // make sure descriptor is not null
         Assertions.assertNotNull(persistedDescriptor);
     }
 
     @Then("App Engine sends a {string} OK response with the descriptor file as a binary payload \\(see OpenAPI spec)")
-    public void app_engine_sends_a_response_with_the_descriptor_file_as_a_binary_payload_see_open_api_spec(String string) {
+    public void appEngineReturnsDescriptorAsBinary(String string) {
         Assertions.assertNotNull(persistedDescriptor);
         JsonNode descriptorJson = DescriptorHelper.parseDescriptor(persistedDescriptor);
         Assertions.assertTrue(descriptorJson.has("namespace"));
@@ -300,7 +310,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Then("App Engine retrieves task outputs with {string}, a {string}  from the database")
-    public void app_engine_retrieves_task_outputs_with_a_from_the_database(String namespace, String version) {
+    public void appEngineRetrievesTaskOutputsByNamespaceAndVersion(String namespace, String version) {
         Assertions.assertNotNull(persistedOutputs);
         Set<Parameter> persistedTaskOutputs = persistedTask
             .getParameters()
@@ -310,8 +320,9 @@ public class ReadTaskStepDefinitions {
         Assertions.assertTrue(TaskTestsUtils.areSetEquals(persistedTaskOutputs, persistedOutputs));
     }
 
-    @Then("App Engine sends a {string} OK response with a payload containing the task outputs as a JSON payload \\(see OpenAPI spec)")
-    public void app_engine_sends_a_ok_response_with_a_payload_containing_the_task_outputs_as_a_json_payload_see_open_api_spec(String string) {
+    @Then("App Engine sends a {string} OK response with a payload containing "
+        + "the task outputs as a JSON payload \\(see OpenAPI spec)")
+    public void appEngineReturnsTaskOutputsAsJson(String string) {
         Assertions.assertNotNull(persistedOutputs);
         Set<Parameter> persistedTaskOutputs = persistedTask
             .getParameters()
@@ -322,7 +333,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Then("App Engine retrieves task outputs with {string} from the database")
-    public void app_engine_retrieves_task_outputs_with_from_the_database(String string) {
+    public void appEngineRetrievesTaskOutputsByUuid(String string) {
         Assertions.assertNotNull(persistedOutputs);
         Set<Parameter> persistedTaskOutputs = persistedTask
             .getParameters()
@@ -333,7 +344,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a task unknown to the App Engine has a {string} and a {string} and a {string}")
-    public void a_task_unknown_to_the_app_engine_has_a_and_a_and_a(String namespace, String version, String uuid) {
+    public void unknownTaskWithNamespaceVersionAndUuid(String namespace, String version, String uuid) {
         // just make sure database is empty and doesn't contain the referenced tasks
         taskRepository.deleteAll();
         persistedNamespace = namespace;
@@ -342,7 +353,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @When("user calls the fetch endpoint {string} with HTTP method {string}")
-    public void user_calls_the_fetch_endpoint_excluding_version_prefix_e_g_with_http_method(String endpoint, String method) {
+    public void userCallsFetchEndpointWithMethod(String endpoint, String method) {
         // call the correct endpoint based on URI
 
         try {
@@ -380,7 +391,10 @@ public class ReadTaskStepDefinitions {
     }
 
     @Then("App Engine sends a {string} HTTP error with a standard error payload containing code {string}")
-    public void app_engine_sends_a_http_error(String expectedStatusCode, String appEngineErrorCode) throws JsonProcessingException {
+    public void appEngineReturnsHttpError(
+        String expectedStatusCode,
+        String appEngineErrorCode
+    ) throws JsonProcessingException {
         // make sure it's a 404 response
         String actualStatusCode = String.valueOf(persistedException.getStatusCode().value());
         Assertions.assertEquals(expectedStatusCode, actualStatusCode);
@@ -414,7 +428,7 @@ public class ReadTaskStepDefinitions {
     }
 
     @Given("a new task run has been created for this task")
-    public void a_task_run_has_been_created_for_this_task() throws FileStorageException {
+    public void taskRunIsCreated() throws FileStorageException {
         persistedRun = new Run(UUID.randomUUID(), TaskRunState.CREATED, null);
         persistedRun = taskRunRepository.save(persistedRun);
         persistedRun.setTask(persistedTask);

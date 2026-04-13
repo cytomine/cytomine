b009b04bda8232c13e51ea97005c3d90cada4e2a
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-04-13 13:35:21 +0200

    wip

---
+++ core/src/main/java/be/cytomine/controller/repository/ReviewedAnnotationController.java
@@ -0,0 +1,78 @@
+package be.cytomine.controller.repository;
+
+import java.util.Optional;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import be.cytomine.common.repository.http.ReviewedAnnotationHttpContract;
+import be.cytomine.common.repository.model.command.payload.response.HttpCommandResponse;
+import be.cytomine.common.repository.model.command.payload.response.ReviewedAnnotationResponse;
+import be.cytomine.common.repository.model.reviewedannotation.payload.CreateReviewedAnnotation;
+import be.cytomine.common.repository.model.reviewedannotation.payload.UpdateReviewedAnnotation;
+import be.cytomine.service.CurrentUserService;
+
+import static java.lang.String.format;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ReviewedAnnotationController {
+
+    private final ReviewedAnnotationHttpContract reviewedAnnotationHttpContract;
+    private final CurrentUserService currentUserService;
+
+    @GetMapping("reviewed_annotation/{id}.json")
+    public ReviewedAnnotationResponse get(@PathVariable long id) {
+        log.debug("REST request to get reviewed annotation {}", id);
+        long userId = currentUserService.getCurrentUser().getId();
+        return reviewedAnnotationHttpContract.findById(id, userId)
+            .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+                format("Unable to find reviewed annotation with id: %s", id)));
+    }
+
+    @GetMapping("reviewed_annotation/count/term/{termId}.json")
+    public long countByTerm(@PathVariable long termId) {
+        log.debug("REST request to count reviewed annotations for term {}", termId);
+        long userId = currentUserService.getCurrentUser().getId();
+        return reviewedAnnotationHttpContract.countByTerm(termId, userId);
+    }
+
+    @PostMapping("reviewed_annotation.json")
+    public Optional<HttpCommandResponse> create(@RequestBody @Valid CreateReviewedAnnotation createReviewedAnnotation) {
+        long userId = currentUserService.getCurrentUser().getId();
+        log.debug("REST request to create reviewed annotation for user {}", userId);
+        return reviewedAnnotationHttpContract.create(userId, createReviewedAnnotation);
+    }
+
+    @PutMapping("reviewed_annotation/{id}.json")
+    public HttpCommandResponse update(@PathVariable long id,
+                                      @RequestBody @Valid UpdateReviewedAnnotation update) {
+        log.debug("REST request to update reviewed annotation {}", id);
+        Optional<HttpCommandResponse> result = reviewedAnnotationHttpContract.update(
+            id, currentUserService.getCurrentUser().getId(), update);
+        return result.orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+            format("Unable to find reviewed annotation with id: %d", id)));
+    }
+
+    @DeleteMapping("reviewed_annotation/{id}.json")
+    public HttpCommandResponse delete(@PathVariable long id) {
+        log.debug("REST request to delete reviewed annotation {}", id);
+        Optional<HttpCommandResponse> result = reviewedAnnotationHttpContract.delete(
+            id, currentUserService.getCurrentUser().getId());
+        return result.orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+            format("Unable to find reviewed annotation with id: %d", id)));
+    }
+}

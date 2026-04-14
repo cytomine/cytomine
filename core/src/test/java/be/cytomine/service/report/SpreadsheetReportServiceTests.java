74148087356f0cfef34a700e4e430397a830ab98
Ba Thien LE <bathien.le.pro@gmail.com>
2026-04-14 09:38:14 +0200

    style: fix lint

--- core/src/test/java/be/cytomine/service/report/SpreadsheetReportServiceTests.java
+++ core/src/test/java/be/cytomine/service/report/SpreadsheetReportServiceTests.java
@@ -1,22 +1,7 @@
 package be.cytomine.service.report;
 
-/*
- * Copyright (c) 2009-2022. Authors: see NOTICE file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -79,8 +64,9 @@ public class SpreadsheetReportServiceTests {
     Object[][] emptyData = {{}};
     String emptyCsvDataResult = "\r\n";
 
-    // @Test ignored for now seems to not work on self-hosted runners
-    public void generate_spreadsheets_with_valid_data_works() {
+    @Disabled("ignored for now seems to not work on self-hosted runners")
+    @Test
+    public void shouldGenerateSpreadsheetSuccessfullyWithValidData() {
         assertEquals(createSpreadsheet(validData), validDataResult);
     }
 

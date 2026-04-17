d9186d0c04a3f2ae157c60bddcf8228594e82fd3
Ba Thien LE <bathien.le.pro@gmail.com>
2026-04-17 15:15:20 +0200

    feat: add bottom drawer

---
+++ web-ui/src/components/appengine/AppBottomDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bottom-drawer box">
+    AppBottomDrawer
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBottomDrawer',
+};
+</script>
+
+<style scoped>
+.bottom-drawer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>

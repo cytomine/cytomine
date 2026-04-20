756bdcfa36d0153b8bbeedd1322b96a31f4f7ff1
sidhamed <siddiqhamed2017@gmail.com>
2026-04-20 12:01:45 +0200

    chore: update imports and annotations in Account.java for consistency

--- core/src/main/java/be/cytomine/dto/Account.java
+++ core/src/main/java/be/cytomine/dto/Account.java
@@ -1,10 +1,15 @@
 package be.cytomine.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-import java.util.List;
 
 @Getter
 @Setter

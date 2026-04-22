d1a8c36459e32fa15d5ad3464c99b58c0274eef8
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-04-22 16:07:10 +0200

    feat: migrate some term persistence layer

--- core/src/test/java/be/cytomine/TermMapper.java
+++ core/src/test/java/be/cytomine/TermMapper.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import be.cytomine.common.repository.model.command.payload.response.TermResponse;
 import be.cytomine.domain.ontology.Term;
@@ -14,6 +15,9 @@ import be.cytomine.domain.ontology.Term;
 @Mapper(componentModel = "spring")
 public interface TermMapper {
 
+    //@BeanMapping(ignoreUnmappedSourceProperties = {"children"})
+    @Mapping(target = "children", ignore = true)
+    @Mapping(target = "ontologyId", source = "ontology.id")
     TermResponse map(Term term);
 
     default Optional<LocalDateTime> map(Date value) {

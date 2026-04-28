0d496bbbed27e9e649ede97926786f1b23d105f4
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-04-28 15:17:00 +0200

    wip

--- sam/tests/conftest.py
+++ sam/tests/conftest.py
@@ -1,4 +1,5 @@
 from typing import Generator
+from unittest.mock import MagicMock, patch
 
 import pytest
 from fastapi.testclient import TestClient
@@ -8,5 +9,6 @@ from app.main import app
 
 @pytest.fixture(scope="module")
 def client() -> Generator[TestClient, None, None]:
-    with TestClient(app) as c:
-        yield c
+    with patch("app.main.load_predictor", return_value=MagicMock()):
+        with TestClient(app) as c:
+            yield c

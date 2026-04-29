f0f718e66829141dc6535b62a7bba74823b5934f
sidhamed <siddiqhamed2017@gmail.com>
2026-04-29 11:32:08 +0200

    fix: simplify ErrorBuilder by returning Error directly

--- core/src/main/java/be/cytomine/controller/error/ErrorBuilder.java
+++ core/src/main/java/be/cytomine/controller/error/ErrorBuilder.java
@@ -3,17 +3,12 @@ package be.cytomine.controller.error;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class ErrorBuilder {
-    public static JsonNode build(ErrorCode code, Map<String, String> details) {
-        ObjectMapper mapper = new ObjectMapper();
-        Error error = ErrorsDictionary.get(code, details);
-        return mapper.valueToTree(error);
+    public static Error build(ErrorCode code, Map<String, String> details) {
+        return ErrorsDictionary.get(code, details);
     }
 
-    public static JsonNode build(ErrorCode code) {
+    public static Error build(ErrorCode code) {
         return build(code, new HashMap<>());
     }
 }

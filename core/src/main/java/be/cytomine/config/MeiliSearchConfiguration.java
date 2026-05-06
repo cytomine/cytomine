4feb2e6dfb24e8925dc0f20849293b9f9b429e41
sidhamed <siddiqhamed2017@gmail.com>
2026-05-06 13:54:47 +0200

    chore: configure MeiliSearch client

---
+++ core/src/main/java/be/cytomine/config/MeiliSearchConfiguration.java
@@ -0,0 +1,23 @@
+package be.cytomine.config;
+
+import com.meilisearch.sdk.Client;
+import com.meilisearch.sdk.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MeiliSearchConfiguration {
+
+    @Value("${meilisearch.host}")
+    private String host;
+
+    @Value("${meilisearch.api-key}")
+    private String apiKey;
+
+    @Bean
+    public Client meilisearchClient() {
+        return new Client(new Config(host, apiKey));
+    }
+
+}

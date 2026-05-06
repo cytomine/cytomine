28132733425030dd4d9eb6a39c8d6137d9690bde
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-06 11:12:22 +0200

    test: add parametrized for primitive outputs

--- app-engine/src/test/java/be/cytomine/appengine/controller/TaskRunControllerTest.java
+++ app-engine/src/test/java/be/cytomine/appengine/controller/TaskRunControllerTest.java
@@ -1,9 +1,14 @@
 package be.cytomine.appengine.controller;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -13,6 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import be.cytomine.appengine.controllers.TaskRunController;
 import be.cytomine.appengine.dto.inputs.task.TaskRunParameterValue;
+import be.cytomine.appengine.dto.inputs.task.types.bool.BooleanValue;
+import be.cytomine.appengine.dto.inputs.task.types.datetime.DateTimeValue;
+import be.cytomine.appengine.dto.inputs.task.types.enumeration.EnumerationValue;
+import be.cytomine.appengine.dto.inputs.task.types.integer.IntegerValue;
+import be.cytomine.appengine.dto.inputs.task.types.number.NumberValue;
+import be.cytomine.appengine.dto.inputs.task.types.string.StringValue;
 import be.cytomine.appengine.handlers.SchedulerHandler;
 import be.cytomine.appengine.models.task.ValueType;
 import be.cytomine.appengine.repositories.RunRepository;
@@ -56,13 +67,43 @@ class TaskRunControllerTest {
         return apiPrefix + apiVersion + "/task-runs";
     }
 
-    @Test
-    void getRunOutputsListShouldReturnOkWithOutputs() throws Exception {
-        String runId = randomUUID().toString();
-        List<TaskRunParameterValue> outputs = List.of(
-            new TaskRunParameterValue(randomUUID(), "output1", ValueType.STRING),
-            new TaskRunParameterValue(randomUUID(), "output2", ValueType.STRING)
+    private static TaskRunParameterValue buildOutput(TaskRunParameterValue value, ValueType type) {
+        value.setTaskRunId(randomUUID());
+        value.setParameterName(randomUUID().toString());
+        value.setType(type);
+        return value;
+    }
+
+    private static Stream<Arguments> primitiveOutputValueProvider() {
+        return Stream.of(
+            Arguments.of(buildOutput(new BooleanValue(true), ValueType.BOOLEAN)),
+            Arguments.of(buildOutput(new IntegerValue(42), ValueType.INTEGER)),
+            Arguments.of(buildOutput(new NumberValue(4.2), ValueType.NUMBER)),
+            Arguments.of(buildOutput(new StringValue("hello"), ValueType.STRING)),
+            Arguments.of(buildOutput(new EnumerationValue("enum"), ValueType.ENUMERATION)),
+            Arguments.of(buildOutput(new DateTimeValue(Instant.now()), ValueType.DATETIME))
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("primitiveOutputValueProvider")
+    void getRunOutputsShouldReturnOkWithPrimitiveOutputs(TaskRunParameterValue output) throws Exception {
+        String runId = randomUUID().toString();
+        List<TaskRunParameterValue> outputs = List.of(output);
+        when(taskProvisioningService.retrieveRunOutputs(runId)).thenReturn(outputs);
+
+        mockMvc.perform(get(baseUrl() + "/" + runId + "/outputs").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json(objectMapper.writeValueAsString(outputs)));
+    }
+
+    @Test
+    void getRunOutputsShouldReturnOkWithAllPrimitiveOutputs() throws Exception {
+        String runId = randomUUID().toString();
+        List<TaskRunParameterValue> outputs = primitiveOutputValueProvider()
+            .map(args -> (TaskRunParameterValue) args.get()[0])
+            .toList();
         when(taskProvisioningService.retrieveRunOutputs(runId)).thenReturn(outputs);
 
         mockMvc.perform(get(baseUrl() + "/" + runId + "/outputs").accept(MediaType.APPLICATION_JSON))

806f3ef7817893cae782700c6537143c566e36a6
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-11 16:13:42 +0200

    feat: check if ontology exist

--- core/src/main/java/be/cytomine/controller/ontology/RestOntologyController.java
+++ core/src/main/java/be/cytomine/controller/ontology/RestOntologyController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import be.cytomine.controller.RestCytomineController;
+import be.cytomine.domain.ontology.Ontology;
 import be.cytomine.service.ontology.OntologyService;
 import be.cytomine.service.utils.TaskService;
 import be.cytomine.utils.JsonObject;
@@ -71,10 +74,13 @@ public class RestOntologyController extends RestCytomineController {
     }
 
     @GetMapping(value = "/ontology/{id}/export", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> export(@PathVariable String id) {
+    public ResponseEntity<Map<String, Object>> export(@PathVariable Long id) {
         log.debug("GET /ontology/{}/export", id);
 
-        String filename = id + ".json";
+        Ontology ontology = ontologyService.find(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Ontology not found with id: " + id));
+
+        String filename = ontology.getName() + ".json";
         return ResponseEntity.ok()
             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
             .body(Map.of());

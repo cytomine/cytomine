7341b74fd0997ee03cd8f7f92addabbb974f8dd0
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-13 16:16:53 +0200

    chore: remove unused code

--- core/src/main/java/be/cytomine/domain/acl/AclEntry.java
+++ core/src/main/java/be/cytomine/domain/acl/AclEntry.java
@@ -75,52 +75,12 @@ public class AclEntry implements Serializable {
         this.id = id;
     }
 
-    public Integer getAceOrder() {
-        return aceOrder;
-    }
-
-    public void setAceOrder(Integer aceOrder) {
-        this.aceOrder = aceOrder;
-    }
-
     public Integer getMask() {
         return mask;
     }
 
     public void setMask(Integer mask) {
         this.mask = mask;
-    }
-
-    public Boolean getGranting() {
-        return granting;
-    }
-
-    public void setGranting(Boolean granting) {
-        this.granting = granting;
-    }
-
-    public Boolean getAuditSuccess() {
-        return auditSuccess;
-    }
-
-    public void setAuditSuccess(Boolean auditSuccess) {
-        this.auditSuccess = auditSuccess;
-    }
-
-    public Boolean getAuditFailure() {
-        return auditFailure;
-    }
-
-    public void setAuditFailure(Boolean auditFailure) {
-        this.auditFailure = auditFailure;
-    }
-
-    public AclObjectIdentity getAclObjectIdentity() {
-        return aclObjectIdentity;
-    }
-
-    public void setAclObjectIdentity(AclObjectIdentity aclObjectIdentity) {
-        this.aclObjectIdentity = aclObjectIdentity;
     }
 
     public AclSid getSid() {

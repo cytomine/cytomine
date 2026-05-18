e25473ee08ce231eecd08fad4ec809ff95f553bb
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-18 12:59:10 +0200

    refactor: using injection dependencies in service project

--- core/src/main/java/be/cytomine/service/project/ProjectDefaultLayerService.java
+++ core/src/main/java/be/cytomine/service/project/ProjectDefaultLayerService.java
@@ -1,28 +1,12 @@
 package be.cytomine.service.project;
 
-/*
- * Copyright (c) 2009-2022. Authors: see NOTICE file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import be.cytomine.domain.CytomineDomain;
@@ -50,24 +34,20 @@ import static org.springframework.security.acls.domain.BasePermission.READ;
 import static org.springframework.security.acls.domain.BasePermission.WRITE;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 @Transactional
 public class ProjectDefaultLayerService extends ModelService {
 
-    @Autowired
-    private ProjectDefaultLayerRepository projectDefaultLayerRepository;
+    private final CurrentUserService currentUserService;
 
-    @Autowired
-    private SecurityACLService securityACLService;
+    private final ProjectDefaultLayerRepository projectDefaultLayerRepository;
 
-    @Autowired
-    private CurrentUserService currentUserService;
+    private final ProjectRepository projectRepository;
 
-    @Autowired
-    private ProjectRepository projectRepository;
+    private final SecurityACLService securityACLService;
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @Override
     public Class currentDomain() {

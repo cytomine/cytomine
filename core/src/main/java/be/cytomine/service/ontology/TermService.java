e99877d627c1d27b0ac11cdfa0bfb1320b3daafc
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-18 12:55:58 +0200

    refactor: using injection dependencies in service ontology

--- core/src/main/java/be/cytomine/service/ontology/TermService.java
+++ core/src/main/java/be/cytomine/service/ontology/TermService.java
@@ -1,21 +1,5 @@
 package be.cytomine.service.ontology;
 
-/*
- * Copyright (c) 2009-2022. Authors: see NOTICE file.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -23,8 +7,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import be.cytomine.common.repository.http.TermHttpContract;
@@ -51,27 +35,22 @@ import be.cytomine.utils.Task;
 import static org.springframework.security.acls.domain.BasePermission.DELETE;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 @Transactional
 public class TermService extends ModelService {
 
-    @Autowired
-    private SecurityACLService securityACLService;
+    private final AnnotationTermRepository annotationTermRepository;
 
-    @Autowired
-    private CurrentUserService currentUserService;
+    private final CurrentUserService currentUserService;
 
-    @Autowired
-    private RelationTermService relationTermService;
+    private final RelationTermService relationTermService;
 
-    @Autowired
-    private AnnotationTermRepository annotationTermRepository;
+    private final ReviewedAnnotationRepository reviewedAnnotationRepository;
 
-    @Autowired
-    private TermHttpContract termHttpContract;
+    private final SecurityACLService securityACLService;
 
-    @Autowired
-    private ReviewedAnnotationRepository reviewedAnnotationRepository;
+    private final TermHttpContract termHttpContract;
 
     @Override
     public Class currentDomain() {
@@ -85,7 +64,6 @@ public class TermService extends ModelService {
     public Optional<TermResponse> find(Long id) {
         return termHttpContract.findTermByID(id, currentUserService.getCurrentUser().getId());
     }
-
 
     public Set<Long> list(Ontology ontology) {
         return termHttpContract.findAllTermIdsByOntology(ontology.getId(), currentUserService.getCurrentUser().getId());
@@ -109,6 +87,7 @@ public class TermService extends ModelService {
      * @param transaction  Transaction link with this command
      * @param task         Task for this command
      * @param printMessage Flag if client will print or not confirm message
+     *
      * @return Response structure (code, old domain,..)
      */
     @Override
@@ -119,7 +98,6 @@ public class TermService extends ModelService {
         Command c = new DeleteCommand(currentUser, transaction);
         return executeCommand(c, domain, null);
     }
-
 
     @Override
     public CytomineDomain createFromJSON(JsonObject json) {

432c4f4781ad970e0329b89d3e1c40ac9591acf9
Ba Thien LE <bathien.le.pro@gmail.com>
2026-05-19 11:34:30 +0200

    test: add geometry parsing tests

--- core/src/test/java/be/cytomine/service/utils/GeometryServiceTests.java
+++ core/src/test/java/be/cytomine/service/utils/GeometryServiceTests.java
@@ -2,6 +2,7 @@ package be.cytomine.service.utils;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.io.geojson.GeoJsonWriter;
@@ -11,15 +12,53 @@ import be.cytomine.domain.annotation.GeometryType;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeometryServiceTests {
+
     private final GeometryService geometryService = new GeometryService(new GeoJsonWriter());
 
-    @Test
-    public void shouldReturnGeometryForValidWkt() {
-        String wkt = "POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))";
+    @Nested
+    class ParseTests {
 
-        Optional<Geometry> result = geometryService.parse(wkt);
+        @Test
+        void shouldReturnGeometryForValidWkt() {
+            String wkt = "POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))";
 
-        assertThat(result).isPresent();
-        assertThat(result.get().getGeometryType()).isEqualTo(GeometryType.POLYGON.getLabel());
+            Optional<Geometry> result = geometryService.parse(wkt);
+
+            assertThat(result).isPresent();
+            assertThat(result.get().getGeometryType()).isEqualTo(GeometryType.POLYGON.getLabel());
+        }
+
+        @Test
+        void shouldReturnGeometryForValidGeoJson() {
+            String geoJson = """
+                {
+                  "type": "Polygon",
+                  "coordinates": [[[0,0],[1,0],[1,1],[0,1],[0,0]]]
+                }
+                """;
+
+            Optional<Geometry> result = geometryService.parse(geoJson);
+
+            assertThat(result).isPresent();
+            assertThat(result.get().getGeometryType()).isEqualTo(GeometryType.POLYGON.getLabel());
+        }
+
+        @Test
+        void shouldReturnEmptyForInvalidInput() {
+            String invalid = "not a geometry";
+
+            Optional<Geometry> result = geometryService.parse(invalid);
+
+            assertThat(result).isEmpty();
+        }
+
+        @Test
+        void shouldReturnEmptyForEmptyString() {
+            String empty = "";
+
+            Optional<Geometry> result = geometryService.parse(empty);
+
+            assertThat(result).isEmpty();
+        }
     }
 }

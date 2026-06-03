c58ff41bed6e16634743a9f3b7fdd72148ac401c
Ba Thien LE <bathien.le.pro@gmail.com>
2026-06-03 15:29:02 +0200

    fix: types

--- pims/pims/utils/types.py
+++ pims/pims/utils/types.py
@@ -1,20 +1,6 @@
-#  * Copyright (c) 2020-2021. Authors: see NOTICE file.
-#  *
-#  * Licensed under the Apache License, Version 2.0 (the "License");
-#  * you may not use this file except in compliance with the License.
-#  * You may obtain a copy of the License at
-#  *
-#  *      http://www.apache.org/licenses/LICENSE-2.0
-#  *
-#  * Unless required by applicable law or agreed to in writing, software
-#  * distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 import json
 from datetime import datetime
-from typing import Any, List, Union
+from typing import Any, Union
 
 
 def parse_json(value: Any, raise_exc: bool = False) -> Union[dict, None]:
@@ -27,8 +13,8 @@ def parse_json(value: Any, raise_exc: bool = False) -> Union[dict, None]:
 
 
 def parse_boolean(value: Any, raise_exc: bool = False) -> Union[bool, None]:
-    _true_set = {'yes', 'true', 't', 'y', '1'}
-    _false_set = {'no', 'false', 'f', 'n', '0'}
+    _true_set = {"yes", "true", "t", "y", "1"}
+    _false_set = {"no", "false", "f", "n", "0"}
 
     if value is True or value is False:
         return value
@@ -65,13 +51,12 @@ def parse_int(value: Any, raise_exc: bool = False) -> Union[int, None]:
 
 
 def parse_datetime(
-    value: Any, formats: List[str] = None, raise_exc: bool = False
+    value: Any,
+    formats: list[str] | None = None,
+    raise_exc: bool = False,
 ) -> Union[datetime, None]:
     if formats is None:
-        formats = [
-            "%Y:%m:%d %H:%M:%S",
-            "%m/%d/%y %H:%M:%S"
-        ]
+        formats = ["%Y:%m:%d %H:%M:%S", "%m/%d/%y %H:%M:%S"]
 
     for format in formats:
         try:
@@ -81,24 +66,6 @@ def parse_datetime(
     if raise_exc:
         raise ValueError
     return None
-
-
-def parse_bytes(
-    value: Any, encoding: str = None, errors: str = 'strict',
-    raise_exc: bool = False
-) -> Union[str, None]:
-    """Return Unicode string from encoded bytes."""
-    try:
-        if encoding is not None:
-            return value.decode(encoding, errors)
-        try:
-            return value.decode('utf-8', errors)
-        except UnicodeDecodeError:
-            return value.decode('cp1252', errors)
-    except:  # noqa
-        if raise_exc:
-            raise ValueError
-        return None
 
 
 def is_int(value: Any) -> bool:

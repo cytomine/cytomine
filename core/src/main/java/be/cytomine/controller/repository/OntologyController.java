34ce3fef9ccf8edf9546b7afe06cd3c00e0332f3
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-06-09 16:39:38 +0200

    chore: upgrade

--- core/src/main/java/be/cytomine/controller/repository/OntologyController.java
+++ core/src/main/java/be/cytomine/controller/repository/OntologyController.java
@@ -1,4 +1,12 @@
 package be.cytomine.controller.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.cytomine.common.repository.http.OntologyHttpContract;
+
+@RequiredArgsConstructor
+@RestController
 public class OntologyController {
+    private final OntologyHttpContract ontologyServiceClient;
 }
--- repository/src/test/java/org/cytomine/repository/http/ReviewedAnnotationControllerTest.java
+++ repository/src/test/java/org/cytomine/repository/http/ReviewedAnnotationControllerTest.java
@@ -1,6 +1,7 @@
 package org.cytomine.repository.http;
 
 import java.util.Set;
+import java.util.UUID;
 
 import lombok.SneakyThrows;
 import org.cytomine.repository.RepositoryApp;
@@ -52,13 +53,15 @@ class ReviewedAnnotationControllerTest {
     @BeforeEach
     void setUp() {
         userId = jdbcTemplate.queryForObject("SELECT nextval('hibernate_sequence')", Long.class);
-        jdbcTemplate.update("INSERT INTO sec_user (id, version, username) VALUES (?, 0, 'admin')", userId);
-        Long adminRoleId = jdbcTemplate.queryForObject("SELECT nextval('hibernate_sequence')", Long.class);
-        jdbcTemplate.update("INSERT INTO sec_role (id, version, authority) VALUES (?, 0, 'ROLE_ADMIN')", adminRoleId);
+        jdbcTemplate.update("INSERT INTO sec_user (id, version, username) VALUES (?, 0, ?)", userId, UUID.randomUUID().toString());
+        jdbcTemplate.update(
+            "INSERT INTO sec_role (id, version, authority) SELECT nextval('hibernate_sequence'), 0, 'ROLE_ADMIN' "
+                + "WHERE NOT EXISTS (SELECT 1 FROM sec_role WHERE authority = 'ROLE_ADMIN')");
         Long userRoleId = jdbcTemplate.queryForObject("SELECT nextval('hibernate_sequence')", Long.class);
         jdbcTemplate.update(
-            "INSERT INTO sec_user_sec_role (id, version, sec_user_id, sec_role_id) " + "VALUES (?, 0, ?, ?)",
-            userRoleId, userId, adminRoleId);
+            "INSERT INTO sec_user_sec_role (id, version, sec_user_id, sec_role_id) SELECT ?, 0, ?, (SELECT id FROM "
+                + "sec_role WHERE authority = 'ROLE_ADMIN')",
+            userRoleId, userId);
 
         Long ontologyId = jdbcTemplate.queryForObject("SELECT nextval('hibernate_sequence')", Long.class);
         jdbcTemplate.update("INSERT INTO ontology (id, version, name, user_id) VALUES (?, 0, 'ontology', ?)",

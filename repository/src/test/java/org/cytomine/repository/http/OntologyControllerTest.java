cf16a966d5762409f5b176561c2bcc0af2d673b4
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-06-12 13:44:37 +0200

    wip

--- repository/src/test/java/org/cytomine/repository/http/OntologyControllerTest.java
+++ repository/src/test/java/org/cytomine/repository/http/OntologyControllerTest.java
@@ -4,11 +4,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import be.cytomine.common.PostGisTestConfiguration;
-import be.cytomine.common.repository.http.OntologyHttpContract;
-import be.cytomine.common.repository.model.command.payload.response.OntologyResponse;
-import be.cytomine.common.repository.model.ontology.payload.CreateOntology;
-import be.cytomine.common.repository.model.ontology.payload.UpdateOntology;
 import lombok.Getter;
 import org.cytomine.repository.RepositoryApp;
 import org.cytomine.repository.mapper.OntologyMapper;
@@ -19,6 +14,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
+
+import be.cytomine.common.PostGisTestConfiguration;
+import be.cytomine.common.repository.http.OntologyHttpContract;
+import be.cytomine.common.repository.model.command.payload.response.OntologyResponse;
+import be.cytomine.common.repository.model.ontology.payload.CreateOntology;
+import be.cytomine.common.repository.model.ontology.payload.UpdateOntology;
 
 @SpringBootTest(classes = RepositoryApp.class)
 @AutoConfigureMockMvc
@@ -40,20 +41,20 @@ public class OntologyControllerTest implements CRUDCommandTests<CreateOntology, 
 
     @Override
     public OntologyResponse expectedUpdatedResponse(OntologyResponse response, UpdateOntology updatePayload,
-            LocalDateTime updated) {
+        LocalDateTime updated) {
         return new OntologyResponse(updatePayload.name().orElse(response.name()), response.id(), response.terms(),
-                response.created(), Optional.of(updated), response.deleted(), response.user());
+            response.created(), Optional.of(updated), response.deleted(), response.user());
     }
 
     @Override
     public OntologyResponse expectedDeletedResponse(OntologyResponse response, LocalDateTime deletedTime) {
         return new OntologyResponse(response.name(), response.id(), response.terms(), response.created(),
-                response.updated(), Optional.of(deletedTime), response.user());
+            response.updated(), Optional.of(deletedTime), response.user());
     }
 
     @Override
     public OntologyResponse expectChangedUpdatedTime(OntologyResponse response, LocalDateTime updatedTime) {
         return new OntologyResponse(response.name(), response.id(), response.terms(), response.created(),
-                Optional.of(updatedTime), response.deleted(), response.user());
+            Optional.of(updatedTime), response.deleted(), response.user());
     }
 }

30b626cc4d41e183b72e27880189b6a28c5ba860
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-06-12 13:44:04 +0200

    wip

--- repository/src/test/java/org/cytomine/repository/http/TermControllerTest.java
+++ repository/src/test/java/org/cytomine/repository/http/TermControllerTest.java
@@ -4,11 +4,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import be.cytomine.common.PostGisTestConfiguration;
-import be.cytomine.common.repository.http.TermHttpContract;
-import be.cytomine.common.repository.model.command.payload.response.TermResponse;
-import be.cytomine.common.repository.model.term.payload.CreateTerm;
-import be.cytomine.common.repository.model.term.payload.UpdateTerm;
 import lombok.Getter;
 import org.cytomine.repository.RepositoryApp;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +14,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
+import be.cytomine.common.PostGisTestConfiguration;
+import be.cytomine.common.repository.http.TermHttpContract;
+import be.cytomine.common.repository.model.command.payload.response.TermResponse;
+import be.cytomine.common.repository.model.term.payload.CreateTerm;
+import be.cytomine.common.repository.model.term.payload.UpdateTerm;
+
 @SpringBootTest(classes = RepositoryApp.class)
 @AutoConfigureMockMvc
 @Import(PostGisTestConfiguration.class)
@@ -27,7 +28,7 @@ class TermControllerTest implements CRUDCommandTests<CreateTerm, TermResponse, U
     String apiURL = TermHttpContract.ROOT_PATH;
     CreateTerm createPayload;
     UpdateTerm updatePayload =
-            new UpdateTerm(Optional.of(UUID.randomUUID().toString()), Optional.of(UUID.randomUUID().toString()));
+        new UpdateTerm(Optional.of(UUID.randomUUID().toString()), Optional.of(UUID.randomUUID().toString()));
     @Autowired
     private MockMvc mockMvc;
 
@@ -42,30 +43,30 @@ class TermControllerTest implements CRUDCommandTests<CreateTerm, TermResponse, U
     public void beforeCreate(long userId) {
         ontologyId = jdbcTemplate.queryForObject("SELECT nextval('hibernate_sequence')", Long.class);
         jdbcTemplate.update("INSERT INTO ontology (id, version, name, user_id) VALUES (?, 0, ?, ?)", ontologyId,
-                UUID.randomUUID(), userId);
+            UUID.randomUUID(), userId);
         createPayload = new CreateTerm(UUID.randomUUID().toString(), UUID.randomUUID().toString(), ontologyId,
-                Optional.of(UUID.randomUUID().toString()));
+            Optional.of(UUID.randomUUID().toString()));
     }
 
     @Override
     public TermResponse expectedUpdatedResponse(TermResponse response, UpdateTerm updatePayload,
-            LocalDateTime updatedTime) {
+        LocalDateTime updatedTime) {
         return new TermResponse(response.id(), updatePayload.name().orElse(response.name()),
-                updatePayload.color().orElse(response.color()), response.ontologyId(), response.created(),
-                Optional.of(updatedTime), response.deleted(), response.comment(), response.children());
+            updatePayload.color().orElse(response.color()), response.ontologyId(), response.created(),
+            Optional.of(updatedTime), response.deleted(), response.comment(), response.children());
     }
 
     @Override
     public TermResponse expectedDeletedResponse(TermResponse response, LocalDateTime deletedTime) {
         return new TermResponse(response.id(), response.name(), response.color(), response.ontologyId(),
-                response.created(), response.updated(), Optional.of(deletedTime), response.comment(),
-                response.children());
+            response.created(), response.updated(), Optional.of(deletedTime), response.comment(),
+            response.children());
     }
 
     @Override
     public TermResponse expectChangedUpdatedTime(TermResponse response, LocalDateTime updatedTime) {
         return new TermResponse(response.id(), response.name(), response.color(), response.ontologyId(),
-                response.created(), Optional.of(updatedTime), response.deleted(), response.comment(),
-                response.children());
+            response.created(), Optional.of(updatedTime), response.deleted(), response.comment(),
+            response.children());
     }
 }

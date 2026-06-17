134e9539f0980971b463f72af6baeb6711a382a6
Ba Thien Le <bathien.le.pro@gmail.com>
2026-06-17 16:33:40 +0200

    refactor(storage): migrate `storage` to repository (#987)
    closes #986

--- common/src/main/java/be/cytomine/common/repository/model/command/request/CommandV2Request.java
+++ common/src/main/java/be/cytomine/common/repository/model/command/request/CommandV2Request.java
@@ -18,7 +18,10 @@ import be.cytomine.common.repository.model.command.payload.request.UpdateCommand
     @JsonSubTypes.Type(value = DeleteTermRelationCommand.class, name = "DELETE_TERM_RELATION_COMMAND"),
     @JsonSubTypes.Type(value = CreateOntologyCommand.class, name = "INSERT_ONTOLOGY_COMMAND"),
     @JsonSubTypes.Type(value = UpdateOntologyCommand.class, name = "UPDATE_ONTOLOGY_COMMAND"),
-    @JsonSubTypes.Type(value = DeleteOntologyCommand.class, name = "DELETE_ONTOLOGY_COMMAND")})
+    @JsonSubTypes.Type(value = DeleteOntologyCommand.class, name = "DELETE_ONTOLOGY_COMMAND"),
+    @JsonSubTypes.Type(value = CreateStorageCommand.class, name = "INSERT_STORAGE_COMMAND"),
+    @JsonSubTypes.Type(value = UpdateStorageCommand.class, name = "UPDATE_STORAGE_COMMAND"),
+    @JsonSubTypes.Type(value = DeleteStorageCommand.class, name = "DELETE_STORAGE_COMMAND")})
 public sealed interface CommandV2Request<T extends HasLongId & HasAclId>
     permits DeleteCommandRequest, CreateCommandRequest, UpdateCommandRequest {
 

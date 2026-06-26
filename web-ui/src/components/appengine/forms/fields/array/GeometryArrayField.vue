619239fc9f0571d53d6f642edd7474efbe4309f9
Ba Thien Le <bathien.le.pro@gmail.com>
2026-06-26 12:37:29 +0200

    fix(task): fix array of geometry inputs (#1024)

---
+++ web-ui/src/components/appengine/forms/fields/array/GeometryArrayField.vue
@@ -0,0 +1,19 @@
+<template>
+  <AnnotationMultiSelect @input="onSelect" />
+</template>
+
+<script>
+import AnnotationMultiSelect from '@/components/appengine/forms/fields/array/AnnotationMultiSelect.vue';
+
+export default {
+  name: 'GeometryArrayField',
+  components: {
+    AnnotationMultiSelect,
+  },
+  methods: {
+    onSelect(ids) {
+      this.$emit('input', {ids, type: 'annotation'});
+    }
+  },
+};
+</script>

08caaf540a437ab595748ec479cb7d5418179702
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-06-30 16:08:13 +0200

    chore: remove redo() operations. use Undo(Undo) instead

--- repository/src/main/java/org/cytomine/repository/service/ApplyCommandService.java
+++ repository/src/main/java/org/cytomine/repository/service/ApplyCommandService.java
@@ -31,7 +31,6 @@ import be.cytomine.common.repository.model.command.request.UpdateTagDomainAssoci
 import be.cytomine.common.repository.model.command.request.UpdateTermCommand;
 import be.cytomine.common.repository.model.command.request.UpdateTermRelationCommand;
 import be.cytomine.common.repository.model.command.request.UpdateUploadedFileCommand;
-import be.cytomine.common.repository.model.storage.payload.CreateStorage;
 
 @Component
 @AllArgsConstructor
@@ -46,60 +45,72 @@ public class ApplyCommandService {
 
     @Transactional
     public Optional<HttpCommandResponse> undoCommand(long userId, UUID undoCommand, LocalDateTime now) {
-        return commandRepository.findById(undoCommand).flatMap(commandEntity -> switch (commandEntity.getData()) {
-            case DeleteTermCommand dtc -> termCommandService.undoDelete(commandEntity.getId(), dtc, userId, now);
-            case CreateTermCommand icr -> termCommandService.undoCreate(commandEntity.getId(), icr, userId, now);
-            case UpdateTermCommand ucr -> termCommandService.undoUpdate(commandEntity.getId(), ucr, userId, now);
-            case DeleteTermRelationCommand deleteTermRelationCommand ->
-                termRelationCommandService.undoDelete(commandEntity.getId(), deleteTermRelationCommand, userId, now);
-            case CreateTermRelationCommand ctrc ->
-                termRelationCommandService.undoCreate(commandEntity.getId(), ctrc, userId, now);
-            case UpdateTermRelationCommand utrc ->
-                termRelationCommandService.undoUpdate(commandEntity.getId(), utrc, userId, now);
-            case CreateOntologyCommand createOntologyCommand ->
-                ontologyCommandService.undoCreate(commandEntity.getId(), createOntologyCommand, userId, now);
-            case DeleteOntologyCommand deleteOntologyCommand ->
-                ontologyCommandService.undoDelete(commandEntity.getId(), deleteOntologyCommand, userId, now);
-            case UpdateOntologyCommand updateOntologyCommand ->
-                ontologyCommandService.undoUpdate(commandEntity.getId(), updateOntologyCommand, userId, now);
-            case CreateStorageCommand csc -> storageCommandService.undoCreate(commandEntity.getId(), csc, userId, now);
-            case UpdateStorageCommand usc -> storageCommandService.undoUpdate(commandEntity.getId(), usc, userId, now);
-            case DeleteStorageCommand dsc -> storageCommandService.undoDelete(commandEntity.getId(), dsc, userId, now);
-            case CreateUploadedFileCommand cufc ->
-                uploadedFileCommandService.undoCreate(commandEntity.getId(), cufc, userId, now);
-            case UpdateUploadedFileCommand uufc ->
-                uploadedFileCommandService.undoUpdate(commandEntity.getId(), uufc, userId, now);
-            case DeleteUploadedFileCommand dufc ->
-                uploadedFileCommandService.undoDelete(commandEntity.getId(), dufc, userId, now);
-            case CreateTagDomainAssociationCommand ctdac ->
-                tagDomainAssociationCommandService.undoCreate(commandEntity.getId(), ctdac, userId, now);
-            case UpdateTagDomainAssociationCommand utdac ->
-                tagDomainAssociationCommandService.undoUpdate(commandEntity.getId(), utdac, userId, now);
-            case DeleteTagDomainAssociationCommand dtdac ->
-                tagDomainAssociationCommandService.undoDelete(commandEntity.getId(), dtdac, userId, now);
-
-            // Actually we undo an undo target here
-            case UndoCreateCommand<?> v -> switch (v.target()) {
-                case CreateTermCommand ctc -> termCommandService.delete(userId, ctc.id(), now);
-                case CreateOntologyCommand coc -> ontologyCommandService.undoCreate(v.commandId(), coc, userId, now);
-                default -> null;
-            };
-            case UndoDeleteCommand<?> v -> switch (v.target()) {
+        return commandRepository.findById(undoCommand)
+            .flatMap(commandEntity -> switch (commandEntity.getData()) {
+                case DeleteTermCommand dtc -> termCommandService.undoDelete(commandEntity.getId(), dtc, userId, now);
+                case CreateTermCommand icr -> termCommandService.undoCreate(commandEntity.getId(), icr, userId, now);
+                case UpdateTermCommand ucr -> termCommandService.undoUpdate(commandEntity.getId(), ucr, userId, now);
+                case DeleteTermRelationCommand deleteTermRelationCommand ->
+                    termRelationCommandService.undoDelete(commandEntity.getId(), deleteTermRelationCommand, userId,
+                        now);
+                case CreateTermRelationCommand ctrc ->
+                    termRelationCommandService.undoCreate(commandEntity.getId(), ctrc, userId, now);
+                case UpdateTermRelationCommand utrc ->
+                    termRelationCommandService.undoUpdate(commandEntity.getId(), utrc, userId, now);
+                case CreateOntologyCommand createOntologyCommand ->
+                    ontologyCommandService.undoCreate(commandEntity.getId(), createOntologyCommand, userId, now);
+                case DeleteOntologyCommand deleteOntologyCommand ->
+                    ontologyCommandService.undoDelete(commandEntity.getId(), deleteOntologyCommand, userId, now);
+                case UpdateOntologyCommand updateOntologyCommand ->
+                    ontologyCommandService.undoUpdate(commandEntity.getId(), updateOntologyCommand, userId, now);
+                case CreateStorageCommand csc ->
+                    storageCommandService.undoCreate(commandEntity.getId(), csc, userId, now);
+                case UpdateStorageCommand usc ->
+                    storageCommandService.undoUpdate(commandEntity.getId(), usc, userId, now);
                 case DeleteStorageCommand dsc ->
-                    storageCommandService.create(userId, new CreateStorage(dsc.before().name()), now);
-                case DeleteTermCommand dtc -> termCommandService.undoDelete(v.commandId(), dtc, userId, now);
-                case DeleteOntologyCommand doc -> ontologyCommandService.undoDelete(v.commandId(), doc, userId, now);
-                default -> null;
-            };
-            case UndoUpdateCommand<?> v -> switch (v.target()) {
-                case UpdateTermCommand updateTermCommand ->
-                    termCommandService.updateWithExistingCommand(userId, v.commandId(), updateTermCommand.getCommand(),
-                        updateTermCommand.after(), now);
-                case UpdateOntologyCommand uoc ->
-                    ontologyCommandService.updateWithExistingCommand(userId, v.commandId(), uoc.getCommand(),
-                        uoc.after(), now);
-                default -> null;
-            };
-        });
+                    storageCommandService.undoDelete(commandEntity.getId(), dsc, userId, now);
+                case CreateUploadedFileCommand cufc ->
+                    uploadedFileCommandService.undoCreate(commandEntity.getId(), cufc, userId, now);
+                case UpdateUploadedFileCommand uufc ->
+                    uploadedFileCommandService.undoUpdate(commandEntity.getId(), uufc, userId, now);
+                case DeleteUploadedFileCommand dufc ->
+                    uploadedFileCommandService.undoDelete(commandEntity.getId(), dufc, userId, now);
+                case CreateTagDomainAssociationCommand ctdac ->
+                    tagDomainAssociationCommandService.undoCreate(commandEntity.getId(), ctdac, userId, now);
+                case UpdateTagDomainAssociationCommand utdac ->
+                    tagDomainAssociationCommandService.undoUpdate(commandEntity.getId(), utdac, userId, now);
+                case DeleteTagDomainAssociationCommand dtdac ->
+                    tagDomainAssociationCommandService.undoDelete(commandEntity.getId(), dtdac, userId, now);
+
+                // Actually we undo an undo target here
+                case UndoCreateCommand<?> v -> switch (v.target()) {
+                    case CreateTermCommand ctc ->
+                        termCommandService.undoDelete(v.commandId(), new DeleteTermCommand(ctc.after(), userId), userId,
+                            now);
+                    case CreateOntologyCommand coc ->
+                        ontologyCommandService.undoDelete(v.commandId(), new DeleteOntologyCommand(coc.after(), userId),
+                            userId, now);
+
+                };
+                case UndoDeleteCommand<?> v -> switch (v.target()) {
+                    case DeleteStorageCommand dsc ->
+                        storageCommandService.undoCreate(v.commandId(), new CreateStorageCommand(dsc.before(), userId),
+                            userId, now);
+                    case DeleteTermCommand dtc ->
+                        termCommandService.undoCreate(v.commandId(), new CreateTermCommand(dtc.before(), userId),
+                            userId, now);
+                    case DeleteOntologyCommand doc -> ontologyCommandService.undoCreate(v.commandId(),
+                        new CreateOntologyCommand(doc.before(), userId), userId, now);
+
+                };
+                case UndoUpdateCommand<?> v -> switch (v.target()) {
+                    case UpdateTermCommand utc -> termCommandService.undoUpdate(v.commandId(),
+                        new UpdateTermCommand(utc.after(), utc.before(), userId), userId, now);
+
+                    case UpdateOntologyCommand uoc -> ontologyCommandService.undoUpdate(v.commandId(),
+                        new UpdateOntologyCommand(uoc.after(), uoc.before(), userId), userId, now);
+
+                };
+            });
     }
 }

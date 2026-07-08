1e505671d9805452105dbbd8c9eca47dd05de297
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-07-08 15:02:14 +0200

    feat: 1050 migrate roles

--- repository/src/main/java/org/cytomine/repository/http/RoleController.java
+++ repository/src/main/java/org/cytomine/repository/http/RoleController.java
@@ -32,7 +32,7 @@ public class RoleController implements RoleHttpContract {
 
     @Override
     public Page<RoleResponse> list(Pageable pageable) {
-        return repository.findAllRoles(pageable).stream().map(mapper::mapToRoleResponse);
+        return repository.findAllRoles(pageable).map(mapper::mapToRoleResponse);
     }
 
     @Override

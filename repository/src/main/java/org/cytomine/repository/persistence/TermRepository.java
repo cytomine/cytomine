d4b751ffb0e75dd03ec1b990d5565c3ab05a05ba
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-08 09:30:58 +0200

    fix: sql queries

--- repository/src/main/java/org/cytomine/repository/persistence/TermRepository.java
+++ repository/src/main/java/org/cytomine/repository/persistence/TermRepository.java
@@ -34,84 +34,100 @@ public interface TermRepository extends JpaRepository<TermEntity, Long> {
         """, nativeQuery = true)
     Set<Long> findAllIdsByProjectId(long projectId);
 
-    @Query(value = """
-        SELECT t.userId AS userId, t.name AS key, count(DISTINCT ua.image_id) AS value, t.color AS color
-        FROM user_annotation ua
-        LEFT JOIN annotation_term at ON at.user_annotation_id = ua.userId
-        LEFT JOIN term t ON t.userId = at.term_id
-        WHERE ua.project_id = :projectId
-        AND (:startDate IS NULL OR at.created > :startDate)
-        AND (:endDate IS NULL OR at.created < :endDate)
-        GROUP BY at.term_id, t.userId, t.name, t.color
-        """, countQuery = """
-        SELECT count(*) FROM (
-            SELECT at.term_id
+    @Query(
+        value = """
+            SELECT t.id AS id, t.name AS name, count(DISTINCT ua.image_id) AS count, t.color AS color
             FROM user_annotation ua
-            LEFT JOIN annotation_term at ON at.user_annotation_id = ua.userId
+            LEFT JOIN annotation_term at ON at.user_annotation_id = ua.id
+            LEFT JOIN term t ON t.id = at.term_id
             WHERE ua.project_id = :projectId
-            AND (:startDate IS NULL OR at.created > :startDate)
-            AND (:endDate IS NULL OR at.created < :endDate)
-            GROUP BY at.term_id
-        ) AS _count
-        """, nativeQuery = true)
-    Page<StatTermProjection> findAllByProjectForStats(long projectId,
+            AND (CAST(:startDate AS timestamp) IS NULL OR at.created > :startDate)
+            AND (CAST(:endDate AS timestamp) IS NULL OR at.created < :endDate)
+            GROUP BY at.term_id, t.id, t.name, t.color
+            """,
+        countQuery = """
+            SELECT count(*) FROM (
+                SELECT at.term_id
+                FROM user_annotation ua
+                LEFT JOIN annotation_term at ON at.user_annotation_id = ua.id
+                WHERE ua.project_id = :projectId
+                AND (CAST(:startDate AS timestamp) IS NULL OR at.created > :startDate)
+                AND (CAST(:endDate AS timestamp) IS NULL OR at.created < :endDate)
+                GROUP BY at.term_id
+            ) AS _count
+            """,
+        nativeQuery = true
+    )
+    Page<StatTermProjection> findAllByProjectForStats(
+        long projectId,
         LocalDateTime startDate,
         LocalDateTime endDate,
-        Pageable pageable);
+        Pageable pageable
+    );
 
-    @Query(value = """
-        SELECT u.userId AS userId, u.username AS key, t.userId AS termId, t.name AS termName,
-               count(at.userId) AS value, t.color AS termColor
-        FROM annotation_term at
-        JOIN user_annotation ua ON ua.userId = at.user_annotation_id
-        JOIN sec_user u ON u.userId = at.user_id
-        JOIN term t ON t.userId = at.term_id
-        JOIN project p ON p.userId = :projectId AND ua.project_id = p.userId
-        WHERE t.ontology_id = p.ontology_id
-        AND t.deleted IS NULL
-        GROUP BY u.userId, u.username, t.userId, t.name, t.color
-        """, countQuery = """
+    @Query(
+        value = """
+            SELECT u.id AS userId, u.username AS username, t.id AS termId, t.name AS termName,
+                   count(at.id) AS termCount, t.color AS termColor
+            FROM annotation_term at
+            JOIN user_annotation ua ON ua.id = at.user_annotation_id
+            JOIN sec_user u ON u.id = at.user_id
+            JOIN term t ON t.id = at.term_id
+            JOIN project p ON p.id = :projectId AND ua.project_id = p.id
+            WHERE t.ontology_id = p.ontology_id
+            AND t.deleted IS NULL
+            GROUP BY u.id, u.username, t.id, t.name, t.color
+            """,
+        countQuery = """
             SELECT count(*) FROM (
                 SELECT at.user_id, at.term_id
                 FROM annotation_term at
-                JOIN user_annotation ua ON ua.userId = at.user_annotation_id
-                JOIN term t ON t.userId = at.term_id
-                JOIN project p ON p.userId = :projectId AND ua.project_id = p.userId
+                JOIN user_annotation ua ON ua.id = at.user_annotation_id
+                JOIN term t ON t.id = at.term_id
+                JOIN project p ON p.id = :projectId AND ua.project_id = p.id
                 WHERE t.ontology_id = p.ontology_id
                 AND t.deleted IS NULL
                 GROUP BY at.user_id, at.term_id
             ) AS _count
-        """, nativeQuery = true)
+            """,
+        nativeQuery = true
+    )
     Page<StatUserTermProjection> findAllByUsersByProjectForStats(long projectId, Pageable pageable);
 
-    @Query(value = """
-        SELECT ua.image_id AS image, at.term_id AS term, COUNT(ua.userId) AS countAnnotations
-        FROM user_annotation ua
-        LEFT JOIN annotation_term at ON at.user_annotation_id = ua.userId
-        WHERE ua.deleted IS NULL
-        AND at.deleted IS NULL
-        AND ua.project_id = :projectId
-        AND (:startDate IS NULL OR at.created > :startDate)
-        AND (:endDate IS NULL OR at.created < :endDate)
-        GROUP BY ua.image_id, at.term_id
-        ORDER BY ua.image_id, at.term_id
-        """, countQuery = """
-        SELECT count(*) FROM (
-            SELECT ua.image_id, at.term_id
+    @Query(
+        value = """
+            SELECT ua.image_id AS imageId, at.term_id AS termId, COUNT(ua.id) AS countAnnotations
             FROM user_annotation ua
-            LEFT JOIN annotation_term at ON at.user_annotation_id = ua.userId
+            LEFT JOIN annotation_term at ON at.user_annotation_id = ua.id
             WHERE ua.deleted IS NULL
             AND at.deleted IS NULL
             AND ua.project_id = :projectId
-            AND (:startDate IS NULL OR at.created > :startDate)
-            AND (:endDate IS NULL OR at.created < :endDate)
+            AND (CAST(:startDate AS timestamp) IS NULL OR at.created > :startDate)
+            AND (CAST(:endDate AS timestamp) IS NULL OR at.created < :endDate)
             GROUP BY ua.image_id, at.term_id
-        ) AS _count
-        """, nativeQuery = true)
-    Page<StatPerTermAndImageProjection> findAllPerTermAndImageByProjectForStats(long projectId,
+            ORDER BY ua.image_id, at.term_id
+            """,
+        countQuery = """
+            SELECT count(*) FROM (
+                SELECT ua.image_id, at.term_id
+                FROM user_annotation ua
+                LEFT JOIN annotation_term at ON at.user_annotation_id = ua.id
+                WHERE ua.deleted IS NULL
+                AND at.deleted IS NULL
+                AND ua.project_id = :projectId
+                AND (CAST(:startDate AS timestamp) IS NULL OR at.created > :startDate)
+                AND (CAST(:endDate AS timestamp) IS NULL OR at.created < :endDate)
+                GROUP BY ua.image_id, at.term_id
+            ) AS _count
+            """,
+        nativeQuery = true
+    )
+    Page<StatPerTermAndImageProjection> findAllPerTermAndImageByProjectForStats(
+        long projectId,
         LocalDateTime startDate,
         LocalDateTime endDate,
-        Pageable pageable);
+        Pageable pageable
+    );
 
     Optional<TermEntity> findByIdAndDeletedNull(long id);
 }

519089ee88412d04315a95db47770a4e3053844b
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-13 11:26:27 +0200

    feat: fetch all commands from the current user

--- web-ui/src/components/user/UserHistoryPage.vue
+++ web-ui/src/components/user/UserHistoryPage.vue
@@ -2,12 +2,24 @@
   <div class="content-wrapper">
     <div class="box">
       <h1>{{ $t('user-history') }}</h1>
+
+      {{ commands }}
     </div>
   </div>
 </template>
 
 <script>
+import {Cytomine} from '@/api';
+
 export default {
   name: 'UserHistoryPage',
+  data() {
+    return {
+      commands: [],
+    };
+  },
+  async created() {
+    this.commands = await await Cytomine.instance.api.get('/commands');
+  },
 };
 </script>

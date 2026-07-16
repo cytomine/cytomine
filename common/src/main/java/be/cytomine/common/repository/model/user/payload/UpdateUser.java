a4916e94b96583cc6c772560ed38959b4192de6a
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-07-16 10:34:27 +0200

    wip

--- common/src/main/java/be/cytomine/common/repository/model/user/payload/UpdateUser.java
+++ common/src/main/java/be/cytomine/common/repository/model/user/payload/UpdateUser.java
@@ -5,8 +5,7 @@ import java.util.Set;
 
 import be.cytomine.common.repository.model.command.payload.response.RoleResponse;
 
-public record UpdateUser(Optional<String> email, Optional<String> name,
-                         Optional<String> firstname, Optional<String> lastname, Optional<String> language,
-                         Optional<String> origin, Optional<Boolean> developer,Optional<String> privateKey,
-                         Optional<String> publicKey, Optional<Set<RoleResponse>> roles) {
-}
+public record UpdateUser(Optional<String> email, Optional<String> name, Optional<String> firstname,
+                         Optional<String> lastname, Optional<String> language, Optional<String> origin,
+                         Optional<Boolean> developer, Optional<String> privateKey, Optional<String> publicKey,
+                         Optional<Set<RoleResponse>> roles) {}

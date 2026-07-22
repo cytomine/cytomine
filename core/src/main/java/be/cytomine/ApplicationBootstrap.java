ae6fe766fb8cf37787b7f928f8e2232db4017475
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-07-22 13:03:28 +0200

    chore: UrlApi spring component

--- core/src/main/java/be/cytomine/ApplicationBootstrap.java
+++ core/src/main/java/be/cytomine/ApplicationBootstrap.java
@@ -22,23 +22,6 @@ import be.cytomine.domain.security.User;
 import be.cytomine.exceptions.ObjectNotFoundException;
 import be.cytomine.repository.meta.ConfigurationRepository;
 import be.cytomine.repository.security.UserRepository;
-import be.cytomine.service.database.BootstrapDataService;
-import be.cytomine.service.database.BootstrapTestsDataService;
-import be.cytomine.service.database.BootstrapUtilsService;
-import be.cytomine.service.utils.Dataset;
-import be.cytomine.utils.EnvironmentUtils;
-
-import static be.cytomine.service.database.BootstrapTestsDataService.ADMIN;
-import static be.cytomine.service.database.BootstrapTestsDataService.CREATOR;
-import static be.cytomine.service.database.BootstrapTestsDataService.GUEST;
-import static be.cytomine.service.database.BootstrapTestsDataService.SUPERADMIN;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_ACL_ADMIN;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_ACL_CREATE;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_ACL_DELETE;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_ACL_READ;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_ACL_WRITE;
-import static be.cytomine.service.database.BootstrapTestsDataService.USER_NO_ACL;
-import be.cytomine.service.UrlApi;
 
 @Component
 @Order(0)
@@ -67,7 +50,7 @@ class ApplicationBootstrap {
         log.info("#############################################################################");
         log.info("#############################################################################");
         String cytomineWelcomeMessage = """
-
+            
                                _____      _                  _
                               / ____|    | |                (_)
                              | |    _   _| |_ ___  _ __ ___  _ _ __   ___
@@ -94,8 +77,6 @@ class ApplicationBootstrap {
         log.info("#############################################################################");
         log.info("#############################################################################");
         log.info("#############################################################################");
-
-        UrlApi.setServerURL(applicationProperties.getServerURL());
 
         if (configurationRepository.findByKey("admin_email").isEmpty()) {
             Configuration configuration = new Configuration();

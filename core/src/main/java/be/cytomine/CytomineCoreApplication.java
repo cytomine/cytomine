99d059a3ab762daa54dd95bd88664c845d6d64e6
Luca <luca.dicarlo@uliege.be>
2026-07-23 16:04:05 +0200

    chore: remove bootstrap app - mongodb (#1078)

--- core/src/main/java/be/cytomine/CytomineCoreApplication.java
+++ core/src/main/java/be/cytomine/CytomineCoreApplication.java
@@ -3,12 +3,16 @@ package be.cytomine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import be.cytomine.config.properties.ApplicationProperties;
 
 @EnableMongoRepositories("be.cytomine.repositorynosql")
 @EnableJpaRepositories("be.cytomine.repository")
 @EntityScan("be.cytomine.domain")
+@EnableConfigurationProperties(ApplicationProperties.class)
 @SpringBootApplication
 public class CytomineCoreApplication {
     public static void main(String[] args) {

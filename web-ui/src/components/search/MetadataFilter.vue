09d0209fe34e596ec4b211550971a52874523760
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-28 16:41:36 +0200

    feat: fetch facets

--- web-ui/src/components/search/MetadataFilter.vue
+++ web-ui/src/components/search/MetadataFilter.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>Filters</div>
+  <div>
+    <h1>Filters</h1>
+
+    <div>{{ facets }}</div>
+  </div>
 </template>
 
 <script lang="js">
+
+import {Cytomine} from '@/api';
+
 export default {
   name: 'MetadataFilter',
+  data() {
+    return {
+      facets: {},
+    };
+  },
+  methods: {
+    async fetchFacets() {
+      try {
+        let {data} = await Cytomine.instance.api.get('meilisearch/facets');
+        this.facets = data || {};
+      } catch (error) {
+        console.log(error);
+        this.error = true;
+      }
+    },
+  },
+  created() {
+    this.fetchFacets();
+  },
 };
 </script>

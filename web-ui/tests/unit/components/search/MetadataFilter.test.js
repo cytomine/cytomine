95c6ab26ba088df2d923c45bc9bb5eff0265ae5b
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-28 14:41:38 +0200

    test: add boilerplate test

---
+++ web-ui/tests/unit/components/search/MetadataFilter.test.js
@@ -0,0 +1,14 @@
+import {shallowMount} from '@vue/test-utils';
+
+import MetadataFilter from '@/components/search/MetadataFilter.vue';
+import {expect} from 'vitest';
+
+describe('MetadataFilter.vue', () => {
+  const createWrapper = () => shallowMount(MetadataFilter, {});
+
+  it('should fetch the data', () => {
+    const wrapper = createWrapper();
+
+    expect(wrapper.text()).toContain('Filters')
+  });
+});

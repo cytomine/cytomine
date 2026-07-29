39b427fd1775326bcb07d060dd2cda94760e48fa
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-29 14:43:13 +0200

    feat: only show filter if facets exist

--- web-ui/src/components/search/MetadataFilter.vue
+++ web-ui/src/components/search/MetadataFilter.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <h1>Metadata filters</h1>
+    <template v-if="Object.keys(facets).length">
+      <h1>Metadata filters</h1>
 
-    <b-field label="Search from metadata">
-      <b-input icon="search" :placeholder="$t('search-placeholder')"/>
-    </b-field>
+      <b-field label="Search from metadata">
+        <b-input icon="search" :placeholder="$t('search-placeholder')"/>
+      </b-field>
 
-    <div>Free text search</div>
+      <div>Free text search</div>
+      <CytomineMultiselect />
 
-    <div>Biological species</div>
+      <div>Biological species</div>
 
-    <div>Anatomical site</div>
+      <div>Anatomical site</div>
 
-    <div>Diagnosis</div>
+      <div>Diagnosis</div>
 
-    <div>Staining</div>
+      <div>Staining</div>
 
-    <b-button icon-left="times">{{ $t('button-clear') }}</b-button>
-
-    <div>{{ facets }}</div>
+      <b-button icon-left="times">{{ $t('button-clear') }}</b-button>
+    </template>
   </div>
 </template>
 
 <script lang="js">
+import CytomineMultiselect from '@/components/form/CytomineMultiselect.vue';
 import { fetchFacets } from '@/utils/search';
 
 export default {
   name: 'MetadataFilter',
+  components: {
+    CytomineMultiselect,
+  },
   data() {
     return {
       facets: {},

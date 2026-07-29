d7d3d5d56def77c1243b85e1760c4aa76f6beb9c
Ba Thien LE <bathien.le.pro@gmail.com>
2026-07-29 16:18:33 +0200

    feat: add search

--- web-ui/src/components/search/MetadataFilter.vue
+++ web-ui/src/components/search/MetadataFilter.vue
@@ -4,7 +4,12 @@
       <h1>Metadata filters</h1>
 
       <b-field label="Search from metadata">
-        <b-input icon="search" :placeholder="$t('search-placeholder')"/>
+        <b-input
+          :value="searchString"
+          @input="debounceSearchString"
+          icon="search"
+          :placeholder="$t('search-placeholder')"
+        />
       </b-field>
 
       <div class="facet-filters">
@@ -18,32 +23,75 @@
         </b-field>
       </div>
 
-      <b-button icon-left="times">{{ $t('button-clear') }}</b-button>
+      <b-button icon-left="times" @click="clear()">{{ $t('button-clear') }}</b-button>
+
+      <div v-if="searched" class="search-results">
+        <h2>{{ results.length }} result(s)</h2>
+
+        <p v-if="!results.length">No result</p>
+        <ul v-else>
+          <li v-for="result in results" :key="result.id">
+            {{ resultLabel(result) }}
+          </li>
+        </ul>
+      </div>
     </template>
   </div>
 </template>
 
 <script lang="js">
+import _ from 'lodash';
+
 import CytomineMultiselect from '@/components/form/CytomineMultiselect.vue';
-import { fetchFacets } from '@/utils/search';
+import { fetchFacets, searchMetadata } from '@/utils/search';
 
 export default {
   name: 'MetadataFilter',
   components: {
     CytomineMultiselect,
   },
+  props: {
+    limit: { type: Number, default: 20 },
+    offset: { type: Number, default: 0 },
+  },
   data() {
     return {
       facets: [],
       selectedFacets: {},
+      searchString: '',
+      results: [],
+      searched: false,
     };
   },
+  computed: {
+    filters() {
+      return Object.entries(this.selectedFacets)
+        .filter(([, values]) => values.length > 0)
+        .map(([key, values]) => {
+          if (values.length === 1) {
+            return `${key}:${values[0]}`;
+          }
+          return `(${values.map(value => `${key} = "${value}"`).join(' OR ')})`;
+        });
+    },
+  },
   watch: {
-
+    filters() {
+      this.search();
+    },
+    searchString() {
+      this.search();
+    },
+    limit() {
+      this.search();
+    },
+    offset() {
+      this.search();
+    },
   },
   methods: {
     async fetchFacets() {
-      let facets = await fetchFacets() || {};
+      let facets = await fetchFacets();
 
       this.facets = Object.entries(facets).map(([key, inner]) => ({
         key,
@@ -51,6 +99,27 @@ export default {
       }));
       this.selectedFacets = Object.fromEntries(this.facets.map(({ key }) => [key, []]));
     },
+    search: _.debounce(async function () {
+      this.results = await searchMetadata({
+        query: this.searchString,
+        filters: this.filters,
+        limit: this.limit,
+        offset: this.offset,
+      });
+      this.searched = true;
+      this.$emit('search', this.results);
+    }, 300),
+    resultLabel(result) {
+      let image = result.image || {};
+      return image.name || image.identifier || result.id;
+    },
+    clear() {
+      this.searchString = '';
+      this.selectedFacets = Object.fromEntries(this.facets.map(({ key }) => [key, []]));
+    },
+    debounceSearchString: _.debounce(function (value) {
+      this.searchString = value;
+    }, 500),
   },
   created() {
     this.fetchFacets();
@@ -84,5 +153,13 @@ export default {
 
 .facet-filters .label {
   overflow-wrap: anywhere;
+}
+
+.metadata-filter .search-results {
+  margin-top: 1rem;
+}
+
+.metadata-filter .search-results ul {
+  list-style: disc inside;
 }
 </style>
--- web-ui/src/utils/search.js
+++ web-ui/src/utils/search.js
@@ -6,5 +6,22 @@ export async function fetchFacets() {
     return data;
   } catch (error) {
     console.error('Failed to fetch facets: ', error);
+    return {};
+  }
+}
+
+export async function searchMetadata({ query = '', filters = [], limit = 20, offset = 0 } = {}) {
+  let params = new URLSearchParams();
+  params.append('query', query);
+  filters.forEach(filter => params.append('filter', filter));
+  params.append('limit', limit);
+  params.append('offset', offset);
+
+  try {
+    let { data } = await Cytomine.instance.api.get('meilisearch/search', { params });
+    return data;
+  } catch (error) {
+    console.error('Failed to search metadata: ', error);
+    return [];
   }
 }

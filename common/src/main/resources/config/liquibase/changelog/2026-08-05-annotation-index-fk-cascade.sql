3e3179a0ebd7742276f360991e4b91076eea3556
Ba Thien LE <bathien.le.pro@gmail.com>
2026-08-06 10:43:34 +0200

    Revert "fix: cascade annotation_index deletes on slice/user deletion"
    This reverts commit cd0ac32e42df131b8a8bba4cb20c2276fedd48cb.

--- common/src/main/resources/config/liquibase/changelog/2026-08-05-annotation-index-fk-cascade.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset cytomine:2026-08-05-annotation-index-fk-cascade
-ALTER TABLE annotation_index DROP CONSTRAINT IF EXISTS fk_q00ycisk5ula612n81sfx3ud2;
-ALTER TABLE annotation_index ADD CONSTRAINT fk_q00ycisk5ula612n81sfx3ud2
-    FOREIGN KEY (slice_id) REFERENCES slice_instance (id) ON DELETE CASCADE;
-ALTER TABLE annotation_index DROP CONSTRAINT IF EXISTS fk_kba5x5ktp61txg1wpagqibsok;
-ALTER TABLE annotation_index ADD CONSTRAINT fk_kba5x5ktp61txg1wpagqibsok
-    FOREIGN KEY (user_id) REFERENCES sec_user (id) ON DELETE CASCADE;

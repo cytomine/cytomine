94208e185e979fc5db5d17da8d6da22d8dc6d74e
Ba Thien LE <bathien.le.pro@gmail.com>
2026-08-10 10:38:59 +0200

    chore: bump vue-color from v2 to v3

--- web-ui/src/components/viewer/panels/colors/CytomineChannel.vue
+++ web-ui/src/components/viewer/panels/colors/CytomineChannel.vue
@@ -14,8 +14,8 @@
     ></div>
     <template #popper v-if="showColorSelector">
       <sketch-picker
-        :value="formattedColor"
-        @input="setColor"
+        :model-value="formattedColor"
+        @update:model-value="setColor"
         :presetColors="presetColors"
         :disable-alpha="true"
       />
@@ -31,12 +31,12 @@
 <script>
 import _ from 'lodash';
 
-import { Sketch } from 'vue-color';
+import { SketchPicker, tinycolor } from 'vue-color';
 
 export default {
   name: 'cytomine-channel',
   components: {
-    'sketch-picker': Sketch,
+    SketchPicker,
   },
   props: {
     name: String,
@@ -95,7 +95,7 @@ export default {
       }
     },
     setColor: _.debounce(function (color) {
-      this.$emit('setColor', color.hex);
+      this.$emit('setColor', tinycolor(color).toHexString());
     }, 500, { leading: true }),
   }
 };
@@ -105,17 +105,17 @@ export default {
   padding: 0 !important;
 }
 
-.color-selector .vc-sketch {
+.color-selector .vc-sketch-picker {
   width: 180px;
 }
 
-.color-selector .vc-sketch-presets-color {
+.color-selector .preset-color {
   width: 1em;
   height: 1em;
   margin: 0 0.25em 0.25em 0;
 }
 
-.color-selector .vc-sketch-presets-color:first-child {
+.color-selector .preset-color:first-child {
   width: 2.25em;
 }
 </style>
--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import constants from '@/utils/constants.js';
 import { getKeycloak } from './keycloak.js';
 
-Vue.configureCompat({ MODE: 2, COMPONENT_V_MODEL: false }); // TODO: remove when migration is done
+Vue.configureCompat({ MODE: 2, COMPONENT_V_MODEL: false }); // TODO: to delete when removing @vue/compat
 
 import i18n from './lang.js';
 import router from './routes.js';
@@ -11,7 +11,7 @@ import store from './store/store.js';
 
 import Buefy from 'buefy';
 import optOutBuefyFromVue2Compat from '@/utils/buefy-compat.js';
-optOutBuefyFromVue2Compat();
+optOutBuefyFromVue2Compat(); // TODO: to delete when removing @vue/compat
 
 import Notifications from '@kyvg/vue3-notification';
 import FloatingVue from 'floating-vue';
@@ -35,6 +35,9 @@ import ViewerOpenLayers from './viewer-ol';
 Vue.use(ViewerOpenLayers);
 
 import 'chart.js/auto';
+import 'vue-color/style.css';
+import optOutVueColorFromVue2Compat from '@/utils/vue-color-compat.js';
+optOutVueColorFromVue2Compat(); // TODO: to delete when removing @vue/compat
 
 import App from './App.vue';
 

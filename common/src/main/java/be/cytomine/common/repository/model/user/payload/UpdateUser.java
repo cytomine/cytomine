f309a007cc9dc616048617543d964dc29e55b7dd
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-08-18 14:28:41 +0200

    finish migrate user migration

--- common/src/main/java/be/cytomine/common/repository/model/user/payload/UpdateUser.java
+++ common/src/main/java/be/cytomine/common/repository/model/user/payload/UpdateUser.java
@@ -6,6 +6,4 @@ import java.util.Optional;
 public record UpdateUser(Optional<String> email, Optional<String> name, Optional<String> firstname,
                          Optional<String> lastname, Optional<String> language, Optional<String> origin,
                          Optional<Boolean> developer, Optional<String> privateKey, Optional<String> publicKey,
-                         Optional<String> role,
-                         Optional<String> password,
-                         String username) {}
+                         Optional<String> role, Optional<String> password, String username) {}

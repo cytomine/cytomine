99d3d5bed979d9dc3204e09099c7bdaa6b3cf73d
Ba Thien LE <bathien.le.pro@gmail.com>
2026-08-19 16:24:35 +0200

    refactor: vue draggable

--- web-ui/src/components/annotations/AnnotationCommentsModal.vue
+++ web-ui/src/components/annotations/AnnotationCommentsModal.vue
@@ -91,7 +91,7 @@ export default {
     },
     annotationURL() {
       let uri = `project/${this.annotation.project}/image/${this.annotation.image}/annotation/${this.annotation.id}`;
-      return `${window.location.origin}/#/${uri}`;
+      return `${window.location.origin}/${uri}`;
     }
   },
   watch: {

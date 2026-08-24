9c254d0db9f9e4bc841dc1805157b97ee9d1f0e1
Ba Thien LE <bathien.le.pro@gmail.com>
2026-08-24 13:50:41 +0200

    chore: remove @vue/compat package

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -1,31 +1,20 @@
-import Vue, { createApp } from 'vue';
-import axios from 'axios';
-import constants from '@/utils/constants.js';
-import { getKeycloak } from './keycloak.js';
-
-Vue.configureCompat({ MODE: 2, COMPONENT_V_MODEL: false }); // TODO: to delete when removing @vue/compat
-
-import i18n from './lang.js';
-import router from './routes.js';
-import store from './store/store.js';
-
-import Buefy from 'buefy';
-import optOutBuefyFromVue2Compat from '@/utils/buefy-compat.js';
-optOutBuefyFromVue2Compat(); // TODO: to delete when removing @vue/compat
-
 import Notifications from '@kyvg/vue3-notification';
-import FloatingVue from 'floating-vue';
-
 import { vOnClickOutside } from '@vueuse/components';
+import axios from 'axios';
+import Buefy from 'buefy';
+import FloatingVue from 'floating-vue';
+import { createApp } from 'vue';
 
-import ViewerOpenLayers from './viewer-ol';
+import App from '@/App.vue';
+import { getKeycloak } from '@/keycloak.js';
+import i18n from '@/lang.js';
+import router from '@/routes.js';
+import store from '@/store/store.js';
+import constants from '@/utils/constants.js';
+import ViewerOpenLayers from '@/viewer-ol';
 
 import 'chart.js/auto';
 import 'vue-color/style.css';
-import optOutVueColorFromVue2Compat from '@/utils/vue-color-compat.js';
-optOutVueColorFromVue2Compat(); // TODO: to delete when removing @vue/compat
-
-import App from './App.vue';
 
 // Load configuration before initializing Keycloak
 axios.get('/configuration.json').then(response => {

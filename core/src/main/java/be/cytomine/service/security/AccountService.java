4e583ee47d3b0b1bf6c1b2fa1f4043bae744c40e
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-09-07 09:39:12 +0200

    wip

--- core/src/main/java/be/cytomine/service/security/AccountService.java
+++ core/src/main/java/be/cytomine/service/security/AccountService.java
@@ -54,6 +54,15 @@ public class AccountService {
     @Value("${keycloak-client.target.realm}")
     private String realm;
 
+    private static void deletePartiallyCreatedAccount(
+        UsersResource usersResource,
+        UserRepresentation user
+    ) {
+        UserRepresentation badAccount =
+            usersResource.searchByUsername(user.getUsername(), true).get(0);
+        usersResource.delete(badAccount.getId());
+    }
+
     public void createAccount(Account account) throws UserManagementException {
 
         log.info("Creating account for user {}", account.username());
@@ -75,7 +84,7 @@ public class AccountService {
             log.info("created account for user {} in IAM", account.username());
         } catch (ServerErrorException e) {
             throw new UserManagementException(ErrorCode.CORE_IAM_UNKNOWN_CREATE_ERROR.toString(), 500);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new UserManagementException(ErrorCode.CORE_IAM_UNKNOWN_CREATE_ERROR.toString(), 500);
         }
 
@@ -184,15 +193,6 @@ public class AccountService {
     ) {
         return keycloak.realm(realm).users().get(userRepresentation.getId()).roles()
             .clientLevel(client.getId()).listAll();
-    }
-
-    private static void deletePartiallyCreatedAccount(
-        UsersResource usersResource,
-        UserRepresentation user
-    ) {
-        UserRepresentation badAccount =
-            usersResource.searchByUsername(user.getUsername(), true).get(0);
-        usersResource.delete(badAccount.getId());
     }
 
     public void update(Account account) {

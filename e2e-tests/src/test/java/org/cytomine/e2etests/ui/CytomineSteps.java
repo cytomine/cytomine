163fa285fba88a00d8c98a5389b64a92966e3c29
Luca Di Carlo <luca.dicarlo@uliege.be>
2026-09-14 16:41:49 +0200

    e2e: with user Roles annotation

--- e2e-tests/src/test/java/org/cytomine/e2etests/ui/CytomineSteps.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/ui/CytomineSteps.java
@@ -648,6 +648,13 @@ public class CytomineSteps {
             new Select(d.findElement(roleSelect)).selectByVisibleText(role);
             return true;
         });
+        if ("Admin".equals(role)) {
+            webDriverUtils.byClick(
+                wait,
+                By.xpath("//label[contains(@class,'b-checkbox')]"
+                    + "[contains(.,'I confirm that I want to make this user an admin')]")
+            );
+        }
     }
 
     public void editUser(
--- e2e-tests/src/test/java/org/cytomine/e2etests/utils/MultiUsersRunner.java
+++ e2e-tests/src/test/java/org/cytomine/e2etests/utils/MultiUsersRunner.java
@@ -52,22 +52,19 @@ public class MultiUsersRunner {
     }
 
     public void run(Wait<WebDriver> wait, List<Role> roles, Consumer<CreatedUser> test) {
-        WebDriver adminDriver = seleniumDriver.driver();
-        Wait<WebDriver> adminWait = new WebDriverWait(adminDriver, Duration.ofSeconds(60));
-        try {
+        for (Role role : roles) {
+            WebDriver adminDriver = seleniumDriver.driver();
+            Wait<WebDriver> adminWait = new WebDriverWait(adminDriver, Duration.ofSeconds(60));
             cytomineSteps.login(adminWait, cytomineUrl, adminUsername, adminPassword);
-            for (Role role : roles) {
-                CreatedUser user = createUser(adminWait, role);
-                try {
-                    cytomineSteps.login(wait, cytomineUrl, user.username(), user.password());
-                    test.accept(user);
-                    cytomineSteps.logout(wait, cytomineUrl);
-                } finally {
-                    keycloakClient.deleteUser(user.username());
-                }
+            CreatedUser user = createUser(adminWait, role);
+            try {
+                cytomineSteps.login(wait, cytomineUrl, user.username(), user.password());
+                test.accept(user);
+                cytomineSteps.logout(wait, cytomineUrl);
+            } finally {
+                keycloakClient.deleteUser(user.username());
             }
             cytomineSteps.logout(adminWait, cytomineUrl);
-        } finally {
             adminDriver.quit();
         }
     }
@@ -75,9 +72,8 @@ public class MultiUsersRunner {
     private CreatedUser createUser(Wait<WebDriver> wait, Role role) {
         String username = "selenium-" + role.name().toLowerCase() + "-" + UUID.randomUUID().toString().substring(0, 8);
         String password = "Selenium1!";
-        cytomineSteps.createUser(
-            wait, cytomineUrl, username, "Selenium", role.name(), username + "@selenium.test", password, label(role)
-        );
+        cytomineSteps.createUser(wait, cytomineUrl, username, "Selenium", role.name(), username + "@selenium.test",
+            password, label(role));
         return new CreatedUser(role, username, password);
     }
 

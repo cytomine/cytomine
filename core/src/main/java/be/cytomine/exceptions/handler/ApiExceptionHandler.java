b0b177e24fc9e678fdcceee21bebe7e462855b90
sidhamed <siddiqhamed2017@gmail.com>
2026-09-14 10:16:33 +0200

    fix: internal server error for unmapped exceptions

--- core/src/main/java/be/cytomine/exceptions/handler/ApiExceptionHandler.java
+++ core/src/main/java/be/cytomine/exceptions/handler/ApiExceptionHandler.java
@@ -216,7 +216,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(SearchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponseDto handleException(SearchException exception) {
         log.debug("SearchException caught: {}", exception.getMessage());
 
